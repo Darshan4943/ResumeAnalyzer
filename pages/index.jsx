75ff8b2fbe143d00f1512dda07266040787b84a8
Darshan <darshanshinde4943@gmaul.com>
2023-11-20 16:13:39 +0530

    tailwind configured

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,10 @@
+import React from 'react';
+
 function Home() {
   return (
     <>
-      <main>
-        <h1>Hello</h1>
+      <main className="bg-gray-100 h-screen flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-red bg-orange ">Hello Tailwind!</h1>
       </main>
     </>
   );
