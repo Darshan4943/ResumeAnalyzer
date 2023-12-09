53f5631dec059e36c4c036d831bb5f4311fddbf4
Darshan <darshanshinde4943@gmaul.com>
2023-12-09 16:13:12 +0530

    changes

--- pages/candidate/afterLogin/jobs/jobs.jsx
+++ pages/candidate/afterLogin/jobs/jobs.jsx
@@ -86,118 +86,119 @@ function Jobs() {
   const [status, setStatus] = React.useState(false);
 
   return (
-    <>
-      <div style={{ backgroundColor: "#E0F6FF" , paddingTop:"10px" }}>
-        <div>
-          <div className="candidate_searchbox customMargins py-4 ">
-            <div className="flex flex-row justify-center items-center w-1128 p-[10px]  pl-[16px] pr-[16px] md:w-full  rounded-lg  bg-white shadow-md">
-              <div className="flex items-center gap-0  p-0  ">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="8%"
-                  height="8%"
-                  viewBox="0 0 42 42"
-                  fill="none"
-                >
-                  <path
-                    d="M27.125 27.125L33.25 33.25L27.125 27.125ZM8.75 19.25C8.75 20.6289 9.02159 21.9943 9.54926 23.2682C10.0769 24.5421 10.8504 25.6996 11.8254 26.6746C12.8004 27.6496 13.9579 28.4231 15.2318 28.9507C16.5057 29.4784 17.8711 29.75 19.25 29.75C20.6289 29.75 21.9943 29.4784 23.2682 28.9507C24.5421 28.4231 25.6996 27.6496 26.6746 26.6746C27.6496 25.6996 28.4231 24.5421 28.9507 23.2682C29.4784 21.9943 29.75 20.6289 29.75 19.25C29.75 16.4652 28.6438 13.7945 26.6746 11.8254C24.7055 9.85625 22.0348 8.75 19.25 8.75C16.4652 8.75 13.7945 9.85625 11.8254 11.8254C9.85625 13.7945 8.75 16.4652 8.75 19.25V19.25Z"
-                    stroke="#333333"
-                    stroke-width="3.1544"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
+    < div className="relative">
 
-                <div>
-                  <input
-                    type="text"
-                    className="text-gray font-small text-[16px] max-scr1400:text-[14px] max-scr1350:text-[13px] max-scr1300:text-[14px] max-scr1250:text-[12px] max-scr1200:text-[11px] max-scr1150:text-[13px] max-scr1100:text-[13px]  max-scr1050:text-[13px]  placeholder-center text-center"
-                    placeholder="Job title or keyword"
-                  />
-                </div>
-              </div>
+      <div className="bg-[#E0F6FF] pt-[10px]">
+        <div className="candidate_searchbox customMargins py-4  ">
+          <div className="flex flex-row justify-center items-center w-1128 p-[10px]  pl-[16px] pr-[16px] md:w-full  rounded-lg  bg-white shadow-md">
+            <div className="flex items-center gap-0  p-0  ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="8%"
+                height="8%"
+                viewBox="0 0 42 42"
+                fill="none"
+              >
+                <path
+                  d="M27.125 27.125L33.25 33.25L27.125 27.125ZM8.75 19.25C8.75 20.6289 9.02159 21.9943 9.54926 23.2682C10.0769 24.5421 10.8504 25.6996 11.8254 26.6746C12.8004 27.6496 13.9579 28.4231 15.2318 28.9507C16.5057 29.4784 17.8711 29.75 19.25 29.75C20.6289 29.75 21.9943 29.4784 23.2682 28.9507C24.5421 28.4231 25.6996 27.6496 26.6746 26.6746C27.6496 25.6996 28.4231 24.5421 28.9507 23.2682C29.4784 21.9943 29.75 20.6289 29.75 19.25C29.75 16.4652 28.6438 13.7945 26.6746 11.8254C24.7055 9.85625 22.0348 8.75 19.25 8.75C16.4652 8.75 13.7945 9.85625 11.8254 11.8254C9.85625 13.7945 8.75 16.4652 8.75 19.25V19.25Z"
+                  stroke="#333333"
+                  stroke-width="3.1544"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
 
-              <img
-                className="w-[3.154px] "
-                src="./images/jobs/searchBarLine.png"
-                alt=""
-              />
-              <div className="flex items-center gap-3 max-scr1100:gap-2 justify-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="6%"
-                  height="6%"
-                  viewBox="0 0 35 34"
-                  fill="none"
-                >
-                  <path
-                    d="M28.3919 14.1666C28.3919 20.4255 17.0586 31.1666 17.0586 31.1666C17.0586 31.1666 5.72522 20.4255 5.72522 14.1666C5.72522 11.1609 6.91926 8.27818 9.04468 6.15277C11.1701 4.02736 14.0528 2.83331 17.0586 2.83331C20.0643 2.83331 22.947 4.02736 25.0724 6.15277C27.1978 8.27818 28.3919 11.1609 28.3919 14.1666V14.1666Z"
-                    stroke="#333333"
-                    stroke-width="3.1544"
-                  />
-                  <path
-                    d="M17.0588 15.5833C17.4345 15.5833 17.7948 15.4341 18.0605 15.1684C18.3262 14.9027 18.4754 14.5424 18.4754 14.1667C18.4754 13.7909 18.3262 13.4306 18.0605 13.1649C17.7948 12.8993 17.4345 12.75 17.0588 12.75C16.683 12.75 16.3227 12.8993 16.057 13.1649C15.7913 13.4306 15.6421 13.7909 15.6421 14.1667C15.6421 14.5424 15.7913 14.9027 16.057 15.1684C16.3227 15.4341 16.683 15.5833 17.0588 15.5833Z"
-                    fill="white"
-                    stroke="#333333"
-                    stroke-width="3.1544"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-                <p className="text-gray font-small text-[16px] max-scr1400:text-[14px]  max-scr1350:text-[13px] max-scr1100:text-[12px]">
-                  Colney, United Kingdom
-                </p>
+              <div>
+                <input
+                  type="text"
+                  className="text-gray font-small text-[16px] max-scr1400:text-[14px] max-scr1350:text-[13px] max-scr1300:text-[14px] max-scr1250:text-[12px] max-scr1200:text-[11px] max-scr1150:text-[13px] max-scr1100:text-[13px]  max-scr1050:text-[13px]  placeholder-center text-center"
+                  placeholder="Job title or keyword"
+                />
               </div>
-              <button className="flex items-center justify-center p-2 px-9 border border-primary bg-blue text-white rounded-[12px] max-scr1200:px-6 max-scr1100:px-">
-                Search
+            </div>
+
+            <img
+              className="w-[3.154px] "
+              src="./images/jobs/searchBarLine.png"
+              alt=""
+            />
+            <div className="flex items-center gap-3 max-scr1100:gap-2 justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="6%"
+                height="6%"
+                viewBox="0 0 35 34"
+                fill="none"
+              >
+                <path
+                  d="M28.3919 14.1666C28.3919 20.4255 17.0586 31.1666 17.0586 31.1666C17.0586 31.1666 5.72522 20.4255 5.72522 14.1666C5.72522 11.1609 6.91926 8.27818 9.04468 6.15277C11.1701 4.02736 14.0528 2.83331 17.0586 2.83331C20.0643 2.83331 22.947 4.02736 25.0724 6.15277C27.1978 8.27818 28.3919 11.1609 28.3919 14.1666V14.1666Z"
+                  stroke="#333333"
+                  stroke-width="3.1544"
+                />
+                <path
+                  d="M17.0588 15.5833C17.4345 15.5833 17.7948 15.4341 18.0605 15.1684C18.3262 14.9027 18.4754 14.5424 18.4754 14.1667C18.4754 13.7909 18.3262 13.4306 18.0605 13.1649C17.7948 12.8993 17.4345 12.75 17.0588 12.75C16.683 12.75 16.3227 12.8993 16.057 13.1649C15.7913 13.4306 15.6421 13.7909 15.6421 14.1667C15.6421 14.5424 15.7913 14.9027 16.057 15.1684C16.3227 15.4341 16.683 15.5833 17.0588 15.5833Z"
+                  fill="white"
+                  stroke="#333333"
+                  stroke-width="3.1544"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+              <p className="text-gray font-small text-[16px] max-scr1400:text-[14px]  max-scr1350:text-[13px] max-scr1100:text-[12px]">
+                Colney, United Kingdom
+              </p>
+            </div>
+            <button className="flex items-center justify-center p-2 px-9 border border-primary bg-blue text-white rounded-[12px] max-scr1200:px-6 max-scr1100:px-">
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="sticky top-[5.6rem]">
+        <div style={{ backgroundColor: "#BCECFF" }} className="">
+          <div className="customMargins">
+            <div className="flex items-center py-5 justify-between">
+              {inputData.map((item, index) => (
+                <InputBox
+                  item={item}
+                  className="text-[14px] font-medium flex items-center w-auto "
+                />
+              ))}
+              <button
+                onClick={toggleChange}
+                className="px-4 py-3  rounded-[6px] bg-[#FFF] "
+              >
+                <img
+                  className="h-[24px] w-[24px]"
+                  src="./images/jobs/fil.png"
+                  alt=""
+                />
               </button>
             </div>
           </div>
+        </div>
 
-          <div style={{ backgroundColor: "#BCECFF" }} className="">
-            <div className="customMargins">
-              <div className="flex items-center py-5 justify-between">
-                {inputData.map((item, index) => (
-                  <InputBox
-                    item={item}
-                    className="text-[14px] font-medium flex items-center w-auto "
-                  />
-                ))}
-                <button
-                  onClick={toggleChange}
-                  className="px-4 py-3  rounded-[6px] bg-[#FFF] "
-                >
-                  <img
-                    className="h-[24px] w-[24px]"
-                    src="./images/jobs/fil.png"
-                    alt=""
-                  />
-                </button>
-              </div>
+        <div style={{ backgroundColor: "#E0F6FF" }}>
+          <div className=" customMargins  ">
+            <div className="flex  items-start gap-9 py-[8px]">
+              <p className={!externalJobs ? "text-[18px] text-black font-semibold border-b-2 border-[#ffda1d]" : "text-[16px] text-black font-medium"} onClick={() => setexternalJobs(false)}>Internal Jobs</p>
+              <p
+                className={externalJobs ? "text-[18px] text-black font-semibold border-b-2 border-[#ffda1d]" : "text-[16px] text-black font-medium"}
+                onClick={() => setexternalJobs(true)}
+              >
+                External Jobs
+              </p>
             </div>
           </div>
-
-          <div style={{ backgroundColor: "#E0F6FF" }}>
-            <div className=" customMargins  ">
-              <div className="flex  items-start gap-9 py-[8px]">
-                <p className={ !externalJobs ? "text-[18px] text-black font-semibold border-b-2 border-[#ffda1d]" : "text-[16px] text-black font-medium"} onClick={() => setexternalJobs(false)}>Internal Jobs</p>
-                <p
-                  className={ externalJobs ? "text-[18px] text-black font-semibold border-b-2 border-[#ffda1d]" : "text-[16px] text-black font-medium"}
-                  onClick={() => setexternalJobs(true)}
-                >
-                  External Jobs
-                </p>
-              </div>
-            </div>
-          </div>
-        {externalJobs && 
+        </div>
+        {externalJobs &&
           <div style={{ backgroundColor: "#f9f9f9" }}>
             <div className=" customMargins  ">
               <div className="inline-flex py-6 justify-center items-center gap-2">
                 {btns.map((item) => (
                   <button className="rounded-full border border-blue bg-white">
                     <p className="font-medium text-[14px] text-black py-2 px-4">
-                      
+
                       {item}
                     </p>
                   </button>
@@ -205,9 +206,8 @@ function Jobs() {
               </div>
             </div>
           </div>
-}
-        </div>
-                
+        }
+
       </div>
       <div className="bg-[#F9F9F9]">
         <div className=" customMargins ">
@@ -761,9 +761,8 @@ function Jobs() {
             )}
             {/* SECOND SECTION   */}
             <div
-              className={`flex flex-col p-3 ${
-                toggle ? "col-span-4" : "col-span-5"
-              } rounded-md border-primary bg-white shadow-md`}
+              className={`flex flex-col p-3 ${toggle ? "col-span-4" : "col-span-5"
+                } rounded-md border-primary bg-white shadow-md`}
               style={{
                 boxShadow: "0px 2px 2px 0px rgba(0, 0, 0, 0.25)",
               }}
@@ -921,9 +920,8 @@ function Jobs() {
 
             {/* LAST SECTION   */}
             <div
-              className={`flex ${
-                toggle ? "col-span-5" : "col-span-7"
-              } flex-col  `}
+              className={`flex ${toggle ? "col-span-5" : "col-span-7"
+                } flex-col  `}
             >
               <div
                 className="p-[16px]   border-[1px] border-[#06A9EF] bg-[#fff] rounded-[8px] flex flex-col gap-[16px]"
@@ -1077,7 +1075,7 @@ function Jobs() {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
