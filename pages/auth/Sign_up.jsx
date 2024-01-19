d1319771d8a084b57bee6d79c5616142d15d4fba
Darshan <darshanshinde4943@gmaul.com>
2024-01-19 21:01:05 +0530

    minor

--- pages/auth/Sign_up.jsx
+++ pages/auth/Sign_up.jsx
@@ -23,7 +23,7 @@ function Sign_up() {
   const router = useRouter();
   return (
     <>
-      <div className="flex justify-center  items-center h-full w-full earth_container mt-[3rem]">
+      <div className="flex justify-center  items-center h-full w-full earth_container my-[3rem]">
         <div className="w-full flex flex-col gap-[36px] ">
           <div>
             <div className="text-[#333] text-center text-[30px] ms:text-[40px] font-[600]">
