3c2dc531d67e6867828a95600a92bc33032c7efa
Nikhil Jadhav <nikhiljadhav3313@gmail.com>
2024-01-22 19:15:01 +0530

    minor

--- pages/employer/afterLogin/EmployerHome.jsx
+++ pages/employer/afterLogin/EmployerHome.jsx
@@ -1,6 +1,5 @@
 // import StackedBarChart from "@/components/common/StackedBarChart";
 import React, { useEffect, useState } from "react";
-import { TablePagination } from "@mui/material";
 
 function EmployerHome({ toggleContentt }) {
   const [page, setPage] = useState(0);
