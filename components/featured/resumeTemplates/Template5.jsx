b79042ec525e278b14457087ceec214b79a271eb
Darshan <darshanshinde4943@gmaul.com>
2024-02-13 19:09:32 +0530

    minor

--- components/featured/resumeTemplates/Template5.jsx
+++ components/featured/resumeTemplates/Template5.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Document, Page, Text, View, Image, StyleSheet, Svg, Path, Rect } from '@react-pdf/renderer';
 function Template5() {
     return (
+        <Page size="A4">
         <View style={{ flexDirection: 'column' }}>
 
             <Text style={{ color: '#414042', fontSize: '16px' }}>
                 Template 5
             </Text>
         </View>
+        </Page>
     )
 }
 
