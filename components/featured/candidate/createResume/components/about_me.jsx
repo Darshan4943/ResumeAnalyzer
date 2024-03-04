e208a275cc71a795a2bf0f992b5d83b77f0b7515
Darshan <darshanshinde4943@gmaul.com>
2024-03-04 14:26:11 +0530

    commit

--- components/featured/candidate/createResume/components/about_me.jsx
+++ components/featured/candidate/createResume/components/about_me.jsx
@@ -11,10 +11,12 @@ const AboutMe = ({ data, setData }) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
   const [isChecked, setIsChecked] = useState(true);
+
   const handleSwitchChange = () => {
     setIsChecked(!isChecked);
     setData({ ...data, showSummary: !isChecked });
   };
+
   const generateText = () => {
     const prompt = `Original Paragraph:\n${text}\n\nNew Paragraph:\n`;
     if (text.length > 100) {
@@ -26,7 +28,7 @@ const AboutMe = ({ data, setData }) => {
           setError("");
           setText(res.data.data.choices[0].message.content);
           localStorage.setItem("attempts", attempt - 1);
-          getAttempts();
+          getAttempts();   
         })
         .catch((err) => {
           setLoading(false);
@@ -36,6 +38,7 @@ const AboutMe = ({ data, setData }) => {
       setError("Minimum 100 characters required");
     }
   };
+
   const getAttempts = () => {
     const localData = localStorage.getItem("attempts");
     if (localData) {
@@ -50,6 +53,7 @@ const AboutMe = ({ data, setData }) => {
       setText(Summery);
     }
   }, [userDataGlobal]);
+
   useEffect(() => {
     setText(data.summery);
   }, [data]);
@@ -79,20 +83,17 @@ const AboutMe = ({ data, setData }) => {
           {loading ? (
             <MiniLoader />
           ) : (
-            <textArea
+            <textarea
               type="text"
               name="aboutMe"
               className="w-full text-[14px] font-montserrat font-small h-full outline-none"
               placeholder="Enter text"
+              maxLength={400} // Set maximum length
               onChange={(e) => {
-                if (e.target.value.length <= 400) {
-                  setText(e.target.value);
-                }
+                setText(e.target.value.slice(0, 400)); // Limit input to 400 characters
               }}
               value={text}
-            >
-              {text}
-            </textArea>
+            />
           )}
         </div>
         <div className="flex items-center justify-between gap-3 w-[100%]  ">
@@ -110,8 +111,8 @@ const AboutMe = ({ data, setData }) => {
           <button
             className=" flex gap-1 items-center font-montserrat text-xs font-semibold btn_outline"
             onClick={generateText}
-            style={{ opacity: text == data?.summery ? 0.5 : 1 }}
-            disabled={text == data?.summery || !isChecked}
+            style={{ opacity: text === data?.summery ? 0.5 : 1 }}
+            disabled={text === data?.summery || !isChecked}
           >
             <SparklingStarts />
             Generate with AI
@@ -119,8 +120,8 @@ const AboutMe = ({ data, setData }) => {
           <button
             className=" font-montserrat text-white font-medium text-[12px] px-[12px] rounded-[8px]  bg-[#06A9EF] w-[60px] h-[32px] "
             onClick={() => setData({ ...data, summery: text })}
-            style={{ opacity: text == data?.summery ? 0.5 : 1 }}
-            disabled={text == data?.summery || !isChecked}
+            style={{ opacity: text === data?.summery ? 0.5 : 1 }}
+            disabled={text === data?.summery || !isChecked}
           >
             Save
           </button>
