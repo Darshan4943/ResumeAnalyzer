ff9c96275b00b78b4345d2bb1bacbae3c24ef507
prasadjadhav1014@gmail.com <dash.sudamait15@gmail.com>
2024-03-11 21:12:24 +0530

    commit

--- pages/purchase/MyPurchase.jsx
+++ pages/purchase/MyPurchase.jsx
@@ -11,7 +11,6 @@ function MyPurchase() {
   const [plan, setPlan] = useState({});
   const [subscription, setSubscription] = useState(null);
   const userDataGlobal = useSelector((state) => state.userData);
-console.log(subscription)
   useEffect(() => {
     axios
       .get("https://freedygoservices.in/api/subscription/" + userDataGlobal._id)
@@ -86,7 +85,13 @@ console.log(subscription)
                             <p className="text-[#0C8A0A]">Status</p>
                             <div className="text-[#0C8A0A]">:</div>
                           </div>
-                          <div className="text-[16px] font-[500] text-[#0C8A0A]">
+                          <div
+                            className={`text-[16px] font-[500] ${
+                              subscription?.isActive
+                                ? "text-[#0C8A0A]"
+                                : "text-red"
+                            }`}
+                          >
                             {subscription?.isActive ? "Active" : "Inactive"}
                           </div>
                         </div>
