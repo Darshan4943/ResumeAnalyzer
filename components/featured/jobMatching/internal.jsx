6cfaa778526a82ea85b6b69f58c1628e01b2c4f0
Darshan <darshanshinde4943@gmaul.com>
2024-03-13 10:38:09 +0530

    commit

--- components/featured/jobMatching/internal.jsx
+++ components/featured/jobMatching/internal.jsx
@@ -57,7 +57,7 @@ const InternalJobMatching = () => {
   const jobMatching = () => {
     setLoading(true);
     axios
-      .post("http://localhost:2000/api/jobMatching/" + userDataGlobal._id, {
+      .post("https://freedygoservices.in/api/jobMatching/" + userDataGlobal._id, {
         jd: text,
         resumeCount
       })
