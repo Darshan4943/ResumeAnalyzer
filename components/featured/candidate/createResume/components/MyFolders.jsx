cbdf09cc9d0e441794d8172e40d8f7285a56f999
Darshan <darshanshinde4943@gmaul.com>
2024-03-20 19:29:59 +0530

    commit

---
+++ components/featured/candidate/createResume/components/MyFolders.jsx
@@ -0,0 +1,129 @@
+import React from 'react'
+
+function MyFolders({ isList, tabIndex, setTabIndex, data, setData, setFolderData, files, setFiles, clientData, setClientData, tab, select, setSelect, selectedIndexes, setSelectedIndexes,openFolder }) {
+    return (
+        <div className=''>
+            {tabIndex === 0 &&
+                <div className='rounded-[16px] p-4 flex flex-row gap-y-6 flex-wrap  w-full h-full'>
+                    {data?.length > 0 ?
+                        <>
+                            {!isList ?
+                                data?.map((item, index) => (
+                                    <>
+
+                                        <div onClick={() => { !select && openFolder(index) }} key={index} className=' break-all flex flex-col items-center text-center cursor-pointer h-fit w-[120px] px-2'>
+                                            <div className=' relative'>
+                                                <svg width="57" height="48" viewBox="0 0 57 48" fill="none" xmlns="http://www.w3.org/2000/svg ">
+                                                    <path d="M50.4997 7.99998H29.7362L27.3944 3.31641C26.8987 2.31758 26.1333 1.47753 25.1847 0.891397C24.2361 0.305266 23.1423 -0.00351467 22.0273 3.01816e-05H6.49996C4.90921 0.00177713 3.38411 0.634475 2.25928 1.75931C1.13445 2.88414 0.501747 4.40924 0.5 5.99999V41.9998C0.501747 43.5905 1.13445 45.1156 2.25928 46.2405C3.38411 47.3653 4.90921 47.998 6.49996 47.9997H50.4997C52.0904 47.998 53.6155 47.3653 54.7404 46.2405C55.8652 45.1156 56.4979 43.5905 56.4996 41.9998V13.9999C56.4979 12.4092 55.8652 10.8841 54.7404 9.75926C53.6155 8.63443 52.0904 8.00173 50.4997 7.99998Z" fill="#4294FF" />
+                                                    <path d="M51.9597 47.7996C51.4854 47.9373 50.9935 48.0047 50.4997 47.9996H6.49996C4.9101 47.995 3.38668 47.3614 2.26248 46.2371C1.13827 45.1129 0.504644 43.5895 0.5 41.9997V5.99989C0.499548 4.7876 0.868792 3.60401 1.55846 2.60702C2.24814 1.61003 3.22544 0.847069 4.35998 0.419922C9.95994 16.1198 23.0599 40.0197 51.9597 47.7996Z" fill="#2965ED" />
+                                                </svg>
+                                                {select &&
+                                                    <input
+                                                        type="checkbox"
+                                                        className=" absolute right-[-15%] top-0 rounded-[4.5px] pl-[4px] pr-[20px] py-[2px] outline-none text-[14px] font-medium custom-checkbox"
+                                                        style={{ width: '20px', height: '20px', }}
+                                                        onClick={(e) => e.stopPropagation()}
+                                                        checked={selectedIndexes.includes(index)}
+                                                        onChange={() => toggleSelect(index)}
+                                                    />
+                                                }
+                                            </div>
+
+
+                                            {item.folderName}
+
+                                        </div>
+
+
+
+                                    </>
+                                ))
+                                :
+                                <table className="w-[100%]" >
+                                    <thead>
+                                        <tr>
+                                            <th className='py-3 px-4 rounded-l-[12px] bg-[#C2E7FF] w-[30%] text-left'>Name</th>
+                                            <th className='py-3 px-4 bg-[#C2E7FF] w-[25%] text-left'>Date Modified</th>
+                                            <th className='py-3 px-4 bg-[#C2E7FF] w-[20%] text-left'>Category</th>
+                                            <th className='py-3 px-4 rounded-r-[12px] bg-[#C2E7FF] w-[25%] text-left'>Size</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody >
+                                        {data.map((item, index) => (
+                                            <tr key={index} onClick={() => openFolder(index)} className=''>
+                                                <td className='px-4 py-2 font-medium flex gap-2 relative items-center '>
+                                                    {select &&
+                                                        <input
+                                                            type="checkbox"
+                                                            className="   rounded-[4.5px]  outline-none text-[14px] font-medium custom-checkbox"
+                                                            style={{ width: '20px', height: '20px', }}
+                                                            onClick={(e) => e.stopPropagation()}
+                                                            checked={selectedIndexes.includes(index)}
+                                                            onChange={() => toggleSelect(index)}
+                                                        />
+                                                    }
+                                                    {item.folderName}
+                                                </td>
+                                                {/* <td className='px-4 py-2 text-[#858585]'>{item.dateModified}</td>
+                            <td className='px-4 py-2 text-[#858585]'>{item.category}</td>
+                            <td className='px-4 py-2 text-[#858585]'>{item.size}</td> */}
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            }
+                        </>
+                        :
+                        <div className='w-full h-full flex flex-col gap-6 justify-center items-center'>
+                            <img
+                                src="/images/noFile.png"
+                                className="h-[300px] w-[375px]"
+                                alt=""
+                            />
+                            <div className='text-[20px] font-medium text-[#808080]'>No Folders Available </div>
+                        </div>
+                    }
+                </div>
+            }
+            {tabIndex === 1 &&
+                <div className=' gap-[1px] p-4 bg-white w-full min-h-[60vh]'>
+                    {files?.length > 0 ?
+                        <table className="w-[100%] ">
+                            <thead>
+                                <tr>
+                                    <th className='py-3 px-4 rounded-l-[12px] bg-[#C2E7FF] w-[30%] text-left'>Name</th>
+                                    <th className='py-3 px-4 bg-[#C2E7FF] w-[25%] text-left'>Date Modified</th>
+                                    <th className='py-3 px-4 bg-[#C2E7FF] w-[20%] text-left'>Category</th>
+                                    <th className='py-3 px-4 rounded-r-[12px] bg-[#C2E7FF] w-[25%] text-left'>Size</th>
+                                </tr>
+                            </thead>
+                            <tbody >
+                                {files.map((item, index) => (
+                                    <tr key={index} className=''>
+                                        <td className='px-4 py-2 font-medium'>{item.fileName}</td>
+                                        {/* <td className='px-4 py-2 text-[#858585]'>{item.dateModified}</td>
+                            <td className='px-4 py-2 text-[#858585]'>{item.category}</td>
+                            <td className='px-4 py-2 text-[#858585]'>{item.size}</td> */}
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+
+                        :
+                        <div className='w-full h-full flex flex-col gap-6 justify-center items-center'>
+                            <img
+                                src="/images/noFile.png"
+                                className="h-[300px] w-[375px]"
+                                alt=""
+                            />
+                            <div className='text-[20px] font-medium text-[#808080]'>No Files Available </div>
+                        </div>
+                    }
+                </div>
+
+            }
+        </div>
+    )
+}
+
+export default MyFolders
--- components/featured/candidate/createResume/components/folders.jsx
+++ components/featured/candidate/createResume/components/folders.jsx
@@ -2,14 +2,16 @@ import Fuse from 'fuse.js';
 import React, { useEffect, useState } from 'react'
 import Files from './files';
 
-function Folders({ tabIndex, setTabIndex, data, setData, clientData, setClientData, tab }) {
+function Folders({ tabIndex, setTabIndex, data, setData, clientData, setClientData, tab,setFolderData,folderData }) {
 
-    const [folderData, setFolderData] = useState([]);
+  
     const [files, setFiles] = useState()
     const [filesAll, setFilesAll] = useState([])
     const [select, setSelect] = useState(false)
     const [selectedFile, setSelectedFile] = useState(null);
-
+    const [selectedIndexes, setSelectedIndexes] = useState([]);
+    const [selectAll, setSelectAll] = useState(false);
+    const [isList,setIsList]= useState(false)
     const handleFileChange = (event, folderName) => {
         const uploadedFiles = event.target.files;
         const newFiles = Array.from(uploadedFiles);
@@ -42,12 +44,20 @@ function Folders({ tabIndex, setTabIndex, data, setData, clientData, setClientDa
             };
             const fuse = new Fuse(filesAll, options);
             const result = fuse.search(value);
-            console.log(45, result);
+           
             setFiles(result.map((item) => item.item));
         } else {
 
             setFiles(filesAll)
         }
+    };
+    const toggleSelectAll = () => {
+        if (selectAll) {
+            setSelectedIndexes([]);
+        } else {
+            setSelectedIndexes(Array.from({ length: clientData.length }, (_, index) => index));
+        }
+        setSelectAll(!selectAll);
     };
 
 
@@ -55,15 +65,14 @@ function Folders({ tabIndex, setTabIndex, data, setData, clientData, setClientDa
         <div className='flex flex-col gap-4 w-[80%] '>
             <div className='flex justify-between'>
                 <p className='text-[24px] font-semibold'>My Collection</p>
-                <div className='bg-[#FFFFFF] rounded-[36px] py-[10px] px-3 flex gap-2 items-center' style={{ boxShadow: "0px 1px 2px 0px #00000040" }}>
-                    <svg width="18" height="14" viewBox="0 0 18 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M6.50033 11.9997H15.667V9.77051H6.50033V11.9997ZM2.33366 4.22884H4.83366V1.99967H2.33366V4.22884ZM2.33366 8.12467H4.83366V5.89551H2.33366V8.12467ZM2.33366 11.9997H4.83366V9.77051H2.33366V11.9997ZM6.50033 8.12467H15.667V5.89551H6.50033V8.12467ZM6.50033 4.22884H15.667V1.99967H6.50033V4.22884ZM2.33366 13.6663C1.87533 13.6663 1.48296 13.5031 1.15658 13.1768C0.830187 12.8504 0.666992 12.458 0.666992 11.9997V1.99967C0.666992 1.54134 0.830187 1.14898 1.15658 0.822591C1.48296 0.496202 1.87533 0.333008 2.33366 0.333008H15.667C16.1253 0.333008 16.5177 0.496202 16.8441 0.822591C17.1705 1.14898 17.3337 1.54134 17.3337 1.99967V11.9997C17.3337 12.458 17.1705 12.8504 16.8441 13.1768C16.5177 13.5031 16.1253 13.6663 15.667 13.6663H2.33366Z" fill="#808080" />
+                <div className='bg-[#FFFFFF] rounded-[36px] py-[10px] px-3 flex gap-2 items-center ' style={{ boxShadow: "0px 1px 2px 0px #00000040" }}>
+                    <svg className='cursor-pointer' onClick={()=>setIsList(true)} width="18" height="14" viewBox="0 0 18 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M6.50033 11.9997H15.667V9.77051H6.50033V11.9997ZM2.33366 4.22884H4.83366V1.99967H2.33366V4.22884ZM2.33366 8.12467H4.83366V5.89551H2.33366V8.12467ZM2.33366 11.9997H4.83366V9.77051H2.33366V11.9997ZM6.50033 8.12467H15.667V5.89551H6.50033V8.12467ZM6.50033 4.22884H15.667V1.99967H6.50033V4.22884ZM2.33366 13.6663C1.87533 13.6663 1.48296 13.5031 1.15658 13.1768C0.830187 12.8504 0.666992 12.458 0.666992 11.9997V1.99967C0.666992 1.54134 0.830187 1.14898 1.15658 0.822591C1.48296 0.496202 1.87533 0.333008 2.33366 0.333008H15.667C16.1253 0.333008 16.5177 0.496202 16.8441 0.822591C17.1705 1.14898 17.3337 1.54134 17.3337 1.99967V11.9997C17.3337 12.458 17.1705 12.8504 16.8441 13.1768C16.5177 13.5031 16.1253 13.6663 15.667 13.6663H2.33366Z" fill={isList ? "#333333" : "#808080"}  />
                     </svg>
                     <div className='h-full w-[1px] bg-[#DEDEDE]'></div>
-                    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M0.5 7.16667V0.5H7.16667V7.16667H0.5ZM0.5 15.5V8.83333H7.16667V15.5H0.5ZM8.83333 7.16667V0.5H15.5V7.16667H8.83333ZM8.83333 15.5V8.83333H15.5V15.5H8.83333ZM2.16667 5.5H5.5V2.16667H2.16667V5.5ZM10.5 5.5H13.8333V2.16667H10.5V5.5ZM10.5 13.8333H13.8333V10.5H10.5V13.8333ZM2.16667 13.8333H5.5V10.5H2.16667V13.8333Z" fill="#333333" />
+                    <svg className='cursor-pointer'  onClick={()=>setIsList(false)} width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0.5 7.16667V0.5H7.16667V7.16667H0.5ZM0.5 15.5V8.83333H7.16667V15.5H0.5ZM8.83333 7.16667V0.5H15.5V7.16667H8.83333ZM8.83333 15.5V8.83333H15.5V15.5H8.83333ZM2.16667 5.5H5.5V2.16667H2.16667V5.5ZM10.5 5.5H13.8333V2.16667H10.5V5.5ZM10.5 13.8333H13.8333V10.5H10.5V13.8333ZM2.16667 13.8333H5.5V10.5H2.16667V13.8333Z" fill={!isList ? "#333333" : "#808080"}  />
                     </svg>
-
 
                 </div>
             </div>
@@ -80,14 +89,16 @@ function Folders({ tabIndex, setTabIndex, data, setData, clientData, setClientDa
                                         <path d="M1.5 10.5L0.5 9.5L4.5 5.5L0.5 1.5L1.5 0.5L5.5 4.5L9.5 0.5L10.5 1.5L6.5 5.5L10.5 9.5L9.5 10.5L5.5 6.5L1.5 10.5Z" fill="#333333" />
                                     </svg>
                                 </div>
-                                <div className='flex gap-4  justify-between w-full '>
+                                <div className='flex  gap-4   justify-between w-full '>
                                     <div className='flex gap-2 text-[14px] font-medium'>
                                         <label className="flex items-center gap-2 text-[14px] font-medium">
                                             Select All
                                             <input
                                                 type="checkbox"
-                                                className=" rounded-[4.5px] pl-[4px] pr-[20px] py-[2px] outline-none text-[14px] font-medium custom-checkbox"
+                                                className=" rounded-[4.5px] pl-[4px] pr-[20px] py-[2px] outline-none text-[14px] font-medium custom-checkbox cursor-pointer"
                                                 style={{ width: '20px', height: '20px',  }}
+                                                checked={selectAll}
+                                                onChange={toggleSelectAll}
                                             />
 
                                         </label>
@@ -126,8 +137,8 @@ function Folders({ tabIndex, setTabIndex, data, setData, clientData, setClientDa
                                         </svg>
 
                                     </div>
-                                    <div className='text-[14px] font-semibold'>
-                                        0 selected
+                                    <div className='text-[14px] font-semibold min-w-[85px] flex justify-end'>
+                                        {selectedIndexes.length} selected
                                     </div>
                                 </div>
 
@@ -216,7 +227,7 @@ function Folders({ tabIndex, setTabIndex, data, setData, clientData, setClientDa
                 </div>
 
                 <div className='h-[1px] w-full bg-[#DEDEDE]'></div>
-                <Files setSelect={setSelect} select={select} setTabIndex={setTabIndex} setFolderData={setFolderData} tabIndex={tabIndex} data={data} setData={setData} files={files} setFiles={setFiles} clientData={clientData} setClientData={setClientData} tab={tab} />
+                <Files isList={isList} selectedIndexes={selectedIndexes} setSelectedIndexes={setSelectedIndexes}  setSelect={setSelect} select={select} setTabIndex={setTabIndex} setFolderData={setFolderData} tabIndex={tabIndex} data={data} setData={setData} files={files} setFiles={setFiles} clientData={clientData} setClientData={setClientData} tab={tab} />
 
             </div>
         </div>
