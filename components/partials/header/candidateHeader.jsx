9484b897322a63e4b2d1bee0c6fc8e34c46c063d
prasadjadhav1014@gmail.com <dash.sudamait15@gmail.com>
2024-04-02 12:38:23 +0530

    commit

--- components/partials/header/candidateHeader.jsx
+++ components/partials/header/candidateHeader.jsx
@@ -12,7 +12,7 @@ function CandidateHeader() {
   const [isLogout, setIsLogout] = useState(false);
   const taskRef = useRef(null);
   const [isLogin, setIsLogin] = useState(false);
-  const [isServices, setServices] = useState(false)
+  const [isServices, setServices] = useState(false);
   useEffect(() => {
     setSelectedPage(router.pathname);
   }, [router.pathname]);
@@ -73,26 +73,37 @@ function CandidateHeader() {
             selectedPage === "/home"
               ? "text-[16px] flex gap-2 items-center bg-[#EAF7FF] p-3 font-semibold rounded-[14px]"
               : " text-[16px] flex gap-2 items-center font-semibold p-3 hover:bg-[#EAF7FF] rounded-[14px] "
-
           }
         >
-
-          <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
             <g mask="url(#mask0_1861_9679)">
-              <path d="M4.99967 15.8333H7.49967V10.8333H12.4997V15.8333H14.9997V8.33333L9.99967 4.58333L4.99967 8.33333V15.8333ZM3.33301 17.5V7.5L9.99967 2.5L16.6663 7.5V17.5H10.833V12.5H9.16634V17.5H3.33301Z" fill="url(#paint0_linear_1861_9679)" />
+              <path
+                d="M4.99967 15.8333H7.49967V10.8333H12.4997V15.8333H14.9997V8.33333L9.99967 4.58333L4.99967 8.33333V15.8333ZM3.33301 17.5V7.5L9.99967 2.5L16.6663 7.5V17.5H10.833V12.5H9.16634V17.5H3.33301Z"
+                fill="url(#paint0_linear_1861_9679)"
+              />
             </g>
             <defs>
-              <linearGradient id="paint0_linear_1861_9679" x1="3.33301" y1="10" x2="16.6663" y2="10" gradientUnits="userSpaceOnUse">
+              <linearGradient
+                id="paint0_linear_1861_9679"
+                x1="3.33301"
+                y1="10"
+                x2="16.6663"
+                y2="10"
+                gradientUnits="userSpaceOnUse"
+              >
                 <stop stop-color="#06A9EF" />
                 <stop offset="1" stop-color="#55CCFF" />
               </linearGradient>
             </defs>
           </svg>
 
-
           <li>Home</li>
-
         </Link>
         <div className="relative ">
           <div
@@ -101,13 +112,28 @@ function CandidateHeader() {
               " text-[16px] flex gap-2  items-center font-semibold p-3 hover:bg-[#EAF7FF] rounded-[14px] cursor-pointer "
             }
           >
-            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-
+            <svg
+              width="20"
+              height="20"
+              viewBox="0 0 20 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
               <g mask="url(#mask0_1861_9684)">
-                <path d="M2.5 9.16667V2.5H9.16667V9.16667H2.5ZM2.5 17.5V10.8333H9.16667V17.5H2.5ZM10.8333 9.16667V2.5H17.5V9.16667H10.8333ZM10.8333 17.5V10.8333H17.5V17.5H10.8333ZM4.16667 7.5H7.5V4.16667H4.16667V7.5ZM12.5 7.5H15.8333V4.16667H12.5V7.5ZM12.5 15.8333H15.8333V12.5H12.5V15.8333ZM4.16667 15.8333H7.5V12.5H4.16667V15.8333Z" fill="url(#paint0_linear_1861_9684)" />
+                <path
+                  d="M2.5 9.16667V2.5H9.16667V9.16667H2.5ZM2.5 17.5V10.8333H9.16667V17.5H2.5ZM10.8333 9.16667V2.5H17.5V9.16667H10.8333ZM10.8333 17.5V10.8333H17.5V17.5H10.8333ZM4.16667 7.5H7.5V4.16667H4.16667V7.5ZM12.5 7.5H15.8333V4.16667H12.5V7.5ZM12.5 15.8333H15.8333V12.5H12.5V15.8333ZM4.16667 15.8333H7.5V12.5H4.16667V15.8333Z"
+                  fill="url(#paint0_linear_1861_9684)"
+                />
               </g>
               <defs>
-                <linearGradient id="paint0_linear_1861_9684" x1="2.5" y1="10" x2="17.5" y2="10" gradientUnits="userSpaceOnUse">
+                <linearGradient
+                  id="paint0_linear_1861_9684"
+                  x1="2.5"
+                  y1="10"
+                  x2="17.5"
+                  y2="10"
+                  gradientUnits="userSpaceOnUse"
+                >
                   <stop stop-color="#06A9EF" />
                   <stop offset="1" stop-color="#55CCFF" />
                 </linearGradient>
@@ -115,13 +141,47 @@ function CandidateHeader() {
             </svg>
 
             <li>Services</li>
-
           </div>
-          {isServices &&
-            <Services setServices={setServices} />
-          }
+          {isServices && <Services setServices={setServices} />}
         </div>
+        <Link
+          href="/jobs/list"
+          className={
+            selectedPage === "/jobs/list"
+              ? "text-[16px] flex gap-2 items-center bg-[#EAF7FF] p-3 font-semibold rounded-[14px]"
+              : " text-[16px] flex gap-2 items-center font-semibold p-3 hover:bg-[#EAF7FF] rounded-[14px] "
+          }
+        >
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g mask="url(#mask0_1861_9679)">
+              <path
+                d="M4.99967 15.8333H7.49967V10.8333H12.4997V15.8333H14.9997V8.33333L9.99967 4.58333L4.99967 8.33333V15.8333ZM3.33301 17.5V7.5L9.99967 2.5L16.6663 7.5V17.5H10.833V12.5H9.16634V17.5H3.33301Z"
+                fill="url(#paint0_linear_1861_9679)"
+              />
+            </g>
+            <defs>
+              <linearGradient
+                id="paint0_linear_1861_9679"
+                x1="3.33301"
+                y1="10"
+                x2="16.6663"
+                y2="10"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stop-color="#06A9EF" />
+                <stop offset="1" stop-color="#55CCFF" />
+              </linearGradient>
+            </defs>
+          </svg>
 
+          <li>Jobs</li>
+        </Link>
         {/* {userDataGlobal.role === "recruiter" && (
           <Link
             href="/myClients"
---
+++ pages/jobs/list/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Index = () => {
+  return <div className="job-list customMargins">jon Listinf</div>;
+};
+
+export default Index;
