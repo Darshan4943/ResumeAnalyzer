606e31a5023f9d835a93594a0e8120507f20b5e8
Sahil Sayyad <Sahil21july@gmail.com>
2024-05-10 19:39:57 +0530

    commit

--- pages/TermsAndConditions.jsx
+++ pages/TermsAndConditions.jsx
@@ -7,11 +7,11 @@ function TermsAndConditions() {
         Terms and Conditions
       </div>
 
-      <div className="py-4 px-6">
-        <h2 className="text-lg font-semibold mb-2">1. Introduction</h2>
-        <p>Welcome to Skilotech! These terms of use {`("Terms")`} govern your access to and use of our digital platforms, including our website {`("Site")`}, web application {`("Web App")`}, and mobile application {`("Mobile App")`} {`(collectively referred to as the "Services")`}. Skilotech s.r.o. {`("Skilotech," "we," or "us")`} provides these Services, and by accessing or using them, you agree to comply with these Terms. If you do not agree with these Terms, please refrain from accessing or using our Services.</p>
+      <div className="py-4 px-6 text-[12px]">
+        <h2 className="text-[16px] font-semibold mb-2">1. Introduction</h2>
+        <p className='mb-2'>Welcome to Skilotech! These terms of use {`("Terms")`} govern your access to and use of our digital platforms, including our website {`("Site")`}, web application {`("Web App")`}, and mobile application {`("Mobile App")`} {`(collectively referred to as the "Services")`}. Skilotech s.r.o. {`("Skilotech," "we," or "us")`} provides these Services, and by accessing or using them, you agree to comply with these Terms. If you do not agree with these Terms, please refrain from accessing or using our Services.</p>
 
-        <h2 className="text-lg font-semibold mb-2">2. License to Use</h2>
+        <h2 className="text-[16px] font-semibold mb-2">2. License to Use</h2>
         <p>You are granted a limited, revocable, non-exclusive, and non-transferable license to access and use the materials available on our Services for personal, non-commercial purposes only. This license does not grant you ownership of any content or materials on the Services and is subject to certain restrictions:</p>
         <ul className="list-disc ml-6">
           <li>You may not modify, reproduce, distribute, transmit, display, perform, publish, license, create derivative works from, or sell any content or materials from the Services.</li>
@@ -20,58 +20,58 @@ function TermsAndConditions() {
           <li>You may not remove or alter any copyright, trademark, or other proprietary notices from the materials.</li>
         </ul>
 
-        <h2 className="text-lg font-semibold mb-2">3. Account</h2>
+        <h2 className="text-[16px] font-semibold mb-2">3. Account</h2>
         <p>To access certain features of our Services, you may need to create an account. By registering for an account, you agree to provide accurate and complete information and to keep your account credentials secure. You are responsible for all activities that occur under your account, and you must notify us immediately of any unauthorized access or use of your account. Skilotech reserves the right to suspend or terminate your account if you violate these Terms or engage in any unauthorized use of the Services.</p>
 
-        <h2 className="text-lg font-semibold mb-2">4. Disclaimer</h2>
+        <h2 className="text-[16px] font-semibold mb-2">4. Disclaimer</h2>
         <p>The materials provided on our Services are for informational purposes only and are provided {`"as is,"`} without any warranties or representations of any kind, whether express or implied. Skilotech disclaims all warranties, including but not limited to, implied warranties of merchantability, fitness for a particular purpose, or non-infringement. Skilotech does not warrant the accuracy, completeness, or reliability of any content or materials on the Services or on any linked sites.</p>
 
-        <h2 className="text-lg font-semibold mb-2">5. Limitations</h2>
+        <h2 className="text-[16px] font-semibold mb-2">5. Limitations</h2>
         <p>In no event shall Skilotech or its suppliers be liable for any damages arising out of or in connection with the use or inability to use the Services, including but not limited to, direct, indirect, incidental, consequential, or punitive damages. This limitation of liability applies even if Skilotech has been advised of the possibility of such damages.</p>
 
-        <h2 className="text-lg font-semibold mb-2">6. Revisions and Errata</h2>
+        <h2 className="text-[16px] font-semibold mb-2">6. Revisions and Errata</h2>
         <p>While we strive to provide accurate and up-to-date information, the materials on our Services may contain technical, typographical, or photographic errors. Skilotech does not warrant that any content or materials on the Services are accurate, complete, or current, and we reserve the right to make changes to the materials at any time without prior notice.</p>
 
-        <h2 className="text-lg font-semibold mb-2">7. Business Partners</h2>
+        <h2 className="text-[16px] font-semibold mb-2">7. Business Partners</h2>
         <p>Skilotech may partner with third-party businesses, such as employers or job recruitment agencies, to provide additional services to our users. These partnerships may involve sharing user data with these partners, subject to user consent. However, Skilotech acts solely as an intermediary in these relationships and assumes no liability for any interactions or transactions between users and third-party partners.</p>
 
-        <h2 className="text-lg font-semibold mb-2">8. Links</h2>
+        <h2 className="text-[16px] font-semibold mb-2">8. Links</h2>
         <p>Our Services may contain links to third-party websites or resources. These links are provided for your convenience only, and Skilotech does not endorse or control the content of these sites. We are not responsible for the availability or accuracy of any linked sites, and your use of such sites is at your own risk.</p>
 
-        <h2 className="text-lg font-semibold mb-2">9. Governing Law</h2>
+        <h2 className="text-[16px] font-semibold mb-2">9. Governing Law</h2>
         <p>These Terms and your use of the Services shall be governed by and construed in accordance with the laws of the Slovak Republic, without regard to its conflict of law principles.</p>
 
-        <h2 className="text-lg font-semibold mb-2">10. Intellectual Property</h2>
+        <h2 className="text-[16px] font-semibold mb-2">10. Intellectual Property</h2>
         <p>All content and materials available on the Services, including but not limited to text, graphics, logos, images, and software, are the property of Skilotech or its licensors and are protected by copyright, trademark, and other intellectual property laws. You may not use, modify, reproduce, distribute, or transmit any content or materials from the Services without prior written permission from Skilotech.</p>
 
-        <h2 className="text-lg font-semibold mb-2">11. Privacy Policy</h2>
+        <h2 className="text-[16px] font-semibold mb-2">11. Privacy Policy</h2>
         <p>Please review our Privacy Policy to understand how we collect, use, and disclose your personal information in connection with the Services.</p>
 
-        <h2 className="text-lg font-semibold mb-2">12. Feedback</h2>
+        <h2 className="text-[16px] font-semibold mb-2">12. Feedback</h2>
         <p>We welcome your feedback and suggestions regarding our Services. Any feedback or suggestions you provide will become the property of Skilotech, and we may use them for any purpose without compensation to you.</p>
 
-        <h2 className="text-lg font-semibold mb-2">13. General Terms for Subscriptions</h2>
+        <h2 className="text-[16px] font-semibold mb-2">13. General Terms for Subscriptions</h2>
         <p>Certain features or services on our Services may be offered on a subscription basis. By subscribing to these services, you agree to the terms and conditions specific to that subscription, including pricing, payment terms, and renewal terms.</p>
 
-        <h2 className="text-lg font-semibold mb-2">14. Communication</h2>
+        <h2 className="text-[16px] font-semibold mb-2">14. Communication</h2>
         <p>We may communicate with you via email, notifications within the Services, or other means regarding your use of the Services, updates, promotions, or other relevant information. You may opt out of receiving certain communications from us by following the instructions provided in the communication or by contacting us directly.</p>
 
-        <h2 className="text-lg font-semibold mb-2">15. Termination</h2>
+        <h2 className="text-[16px] font-semibold mb-2">15. Termination</h2>
         <p>You may terminate your account or subscription at any time by following the instructions provided in the Services. Skilotech reserves the right to suspend or terminate your access to the Services at any time without prior notice if we believe you have violated these Terms or engaged in any unauthorized use of the Services.</p>
 
-        <h2 className="text-lg font-semibold mb-2">16. No Third-Party Beneficiaries</h2>
+        <h2 className="text-[16px] font-semibold mb-2">16. No Third-Party Beneficiaries</h2>
         <p>These Terms are solely for the benefit of Skilotech and its users and do not create any rights or benefits for any third parties.</p>
 
-        <h2 className="text-lg font-semibold mb-2">17. Modifications to Site</h2>
+        <h2 className="text-[16px] font-semibold mb-2">17. Modifications to Site</h2>
         <p>Skilotech reserves the right to modify, suspend, or discontinue any part of the Services at any time without prior notice. We may also impose limits on certain features or restrict your access to parts of the Services without liability or obligation to you.</p>
 
-        <h2 className="text-lg font-semibold mb-2">18. Severability</h2>
+        <h2 className="text-[16px] font-semibold mb-2">18. Severability</h2>
         <p>If any provision of these Terms is found to be invalid or unenforceable, that provision shall be deemed severable from these Terms and shall not affect the validity and enforceability of the remaining provisions.</p>
 
-        <h2 className="text-lg font-semibold mb-2">19. Updates to the Terms</h2>
+        <h2 className="text-[16px] font-semibold mb-2">19. Updates to the Terms</h2>
         <p>Skilotech may update or revise these Terms from time to time without prior notice. Any changes to these Terms will be effective immediately upon posting the revised Terms on the Services. Your continued use of the Services after the posting of the revised Terms constitutes your acceptance of the changes.</p>
 
-        <h2 className="text-lg font-semibold mb-2">20. Questions</h2>
+        <h2 className="text-[16px] font-semibold mb-2">20. Questions</h2>
         <p>If you have any questions or concerns about these Terms or our Services, please contact us at Support@Skilotech.com.</p>
       </div>
     </div>
