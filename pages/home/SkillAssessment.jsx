b4f96e5203a510511fdbc17f4ec646ac5f53dc53
Pranay Salunke <pranaysalunke5@gmail.com>
2024-05-20 14:34:39 +0530

    commit

--- pages/home/SkillAssessment.jsx
+++ pages/home/SkillAssessment.jsx
@@ -66,7 +66,7 @@ function SkillAssessment() {
   ]);
   const [uniqueQuestions, setUniqueQuestions] = useState([]);
 
-              // START 
+  // START
   const [skills, setSkills] = useState([]);
   const [skillList, setSkillList] = useState([]);
 
@@ -74,15 +74,54 @@ function SkillAssessment() {
     axios
       .get("http://localhost:2000/api/AllSkills")
       .then((res) => {
-        const names = res.data.map(skill => skill.name);
-       
+        const names = res.data.map((skill) => skill.name);
+
         setSkills(names);
-    })
+      })
       .catch((err) => {
         console.log(err);
       });
   }, [skillList]);
 
+  const handleInputChange = async (selectedOption) => {
+    const found = skillList?.find(
+      (item) => item.skill.name === selectedOption.label
+    );
+    if (!found) {
+      try {
+        const response = await fetch("http://localhost:2000/api/skills", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ name: selectedOption.label }),
+        });
+
+        if (response.ok) {
+          const newSkill = { skill: selectedOption.label };
+          setSkillList([...skillList, newSkill]);
+          setSelectedSkill(selectedOption.value);
+          setInputValue(selectedOption.value);
+          
+        } else {
+          console.error("Failed to add skill:", response.statusText);
+        }
+      } catch (error) {
+        console.error("Error adding skill:", error.message);
+      }
+    }
+  };
+  const handleLevelChange = (event) => {
+    setLevel(event.target.value);
+  };
+
+  // const handleInputChange = (selectedOption) => {
+  //   setSelectedSkill(selectedOption.value);
+  //   setInputValue(selectedOption.value);
+  // };
+  // const handleLevelChange = (event) => {
+  //   setLevel(event.target.value);
+  // };
 
   useEffect(() => {
     const uniqueQuestionsSet = new Set();
@@ -101,10 +140,10 @@ function SkillAssessment() {
 
     // while (filteredQuestions.length < 10) {
     //   toggleContent(); // This function should generate a new unique question
-    //   // if (!uniqueQuestionsSet.has(newQuestion.question)) {
-    //   //   uniqueQuestionsSet.add(newQuestion.question);
-    //   //   filteredQuestions.push(newQuestion);
-    //   // }
+    // if (!uniqueQuestionsSet.has(newQuestion.question)) {
+    //   uniqueQuestionsSet.add(newQuestion.question);
+    //   filteredQuestions.push(newQuestion);
+    // }
     // }
 
     if (filteredQuestions.length < 10 && selectedSkill) {
@@ -113,13 +152,14 @@ function SkillAssessment() {
   }, [question]);
 
   const [isSkiped, setIsSkiped] = useState(false);
-  const handleInputChange = (selectedOption) => {
-    setSelectedSkill(selectedOption.value);
-    setInputValue(selectedOption.value);
-  };
-  const handleLevelChange = (event) => {
-    setLevel(event.target.value);
-  };
+
+  // const handleInputChange = (selectedOption) => {
+  //   setSelectedSkill(selectedOption.value);
+  //   setInputValue(selectedOption.value);
+  // };
+  // const handleLevelChange = (event) => {
+  //   setLevel(event.target.value);
+  // };
 
   useEffect(() => {
     axios
@@ -398,13 +438,12 @@ function SkillAssessment() {
                         </div>
                       </div>
                     </div>
-                 
+
                     <CreatableSelect
-                        options={skills.map((item) => ({
-                          value: item,
-                          label: item,
-                        }))}
-                     
+                      options={skills.map((item) => ({
+                        value: item,
+                        label: item,
+                      }))}
                       className="w-full"
                       onChange={handleInputChange}
                     />
