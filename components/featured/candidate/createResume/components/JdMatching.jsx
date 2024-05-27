e265089f99ee6caac4ef7a9f35bf4b002a4d93cd
Prathmesh Jadhav <prasadjadhav1014@gmail.com>
2024-05-27 14:20:58 +0530

    commit

--- components/featured/candidate/createResume/components/JdMatching.jsx
+++ components/featured/candidate/createResume/components/JdMatching.jsx
@@ -222,11 +222,11 @@ const JdMatching = ({ details, resumeList, isAnimate }) => {
                               <div className="flex gap-2 justify-end">
                                 <div className="flex flex-row gap-[8px] items-center">
                                   {
-                                    console.log(data?.resumeUrl)
+                                    console.log(124,data)
                                   }
                                   <svg
                                     onClick={() =>
-                                      (window.location.href = data?.resumeUrl)
+                                      (window.location.href = data?.file)
                                     }
                                     width="14"
                                     height="14"
