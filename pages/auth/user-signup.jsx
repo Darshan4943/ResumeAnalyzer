d9c3f58e8817c68d26baff6cf6c740420e261273
Darshan <darshanshinde4943@gmaul.com>
2024-05-29 12:46:31 +0530

    commit

--- pages/auth/user-signup.jsx
+++ pages/auth/user-signup.jsx
@@ -158,7 +158,7 @@ function UserSignUp({ setIsSignIn, setSignIn, setSignUp }) {
 
   function validatePassword(password) {
     const strongPasswordRegex =
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{4,}$/;
     return strongPasswordRegex.test(password);
   }
 
