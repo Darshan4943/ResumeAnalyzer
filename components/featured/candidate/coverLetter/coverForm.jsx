b52aa328490c99d036c0d6a59c072f107ad63bbc
Darshan <darshanshinde4943@gmaul.com>
2024-06-13 18:04:43 +0530

    commit

--- components/featured/candidate/coverLetter/coverForm.jsx
+++ components/featured/candidate/coverLetter/coverForm.jsx
@@ -31,6 +31,7 @@ function CoverForm({
   const [letterData, setLetterData] = useState("");
   const [isShow, setIsShow] = useState(true);
   const [contentSituation, setContentSituation] = useState("Experienced");
+  const [loading, setLoading] = useState(false);
   const handleImageClick = (template) => {
     togglePreview(true, template.index);
     setSelectedColor(template.themeColor);
@@ -60,7 +61,7 @@ function CoverForm({
     ));
   };
 
-  const handleSaveData=()=>{
+  const handleSaveData = () => {
     console.log(7865)
   }
   const renderAllTemplates = () => {
@@ -78,21 +79,16 @@ function CoverForm({
       />
     ));
   };
-
-  const addCoverLetter = async () => {
+  const fetchCoverLetter = async () => {
+    setLoading(true)
     try {
-      const formData = { ...data, userId: userDataGlobal._id };
-
-      const response = await axios.post(
-        "http://localhost:2000/api/cover/add",
-        formData
-      );
-      toast.success("Cover Letter added successfully");
-      return response.data;
+      const response = await axios.post('http://localhost:2000/api/cover-letter/transform', data);
+      const letterData = response.data;
+      setData({ ...data, passages: letterData.passages });
+      setLoading(false)
     } catch (error) {
-      console.error("Error adding cover letter:", error);
-      toast.error("Error adding cover letter");
-      throw error;
+      console.error('Error fetching cover letter:', error);
+      setLoading(false)
     }
   };
 
@@ -280,21 +276,19 @@ function CoverForm({
             <div className="bg-[#DEDEDE] w-full h-[1px]"> </div>
             <div className="bg-[#F9F9F9] w-full flex rounded-[8px] text-[14px] font-semibold">
               <button
-                className={`${
-                  isFormat === "standard"
+                className={`${isFormat === "standard"
                     ? "bg-[#06A9EF] py-[8px] px-[16px] flex justify-center items-center rounded-[8px] w-[50%] text-white"
                     : "py-[8px] px-[16px] flex justify-center items-center rounded-[8px] w-[50%]"
-                }`}
+                  }`}
                 onClick={() => setIsFormat("standard")}
               >
                 Standard Format
               </button>
               <button
-                className={`${
-                  isFormat === "custom"
+                className={`${isFormat === "custom"
                     ? "bg-[#06A9EF] py-[8px] px-[16px] flex justify-center items-center rounded-[8px] w-[50%] text-white"
                     : "py-[8px] px-[16px] flex justify-center items-center rounded-[8px] w-[50%]"
-                }`}
+                  }`}
                 onClick={() => setIsFormat("custom")}
               >
                 Custom Format
@@ -417,10 +411,30 @@ function CoverForm({
           <div className="flex justify-end ">
             {isFormat === "standard" && (
               <button
-                className="bg-[#06A9EF] py-[8px] px-[16px] text-[12px] flex justify-center items-center rounded-[8px]  text-white"
-                onClick={handleSaveData}
+                className="bg-[#06A9EF] w-[126px] h-[34px] py-[8px] px-[16px] text-[12px] flex justify-center items-center rounded-[8px]  text-white"
+                onClick={fetchCoverLetter}
               >
-                Generate Letter
+                {loading ? (
+                  <svg
+                    aria-hidden="true"
+                    role="status"
+                    className="inline w-4 h-4  animate-spin"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="#E5E7EB"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                ) : (
+                  "Generate Letter"
+                )}
               </button>
             )}
             {isFormat === "custom" && (
@@ -462,7 +476,7 @@ function CoverForm({
             <div className="flex flex-row justify-between items-center  gap-[10px] ">
               <button
                 className="flex  items-center font-montserrat text-xs font-semibold btn_outline gap-[6px]"
-                // onClick={generateText}
+              // onClick={generateText}
               >
                 <SparklingStarts />
                 <span className="text-[12px] text-[#333333] font-[600] font-Montserrat leading-[16px]">
@@ -472,8 +486,8 @@ function CoverForm({
               <button
                 className={`font-montserrat text-white font-medium text-[12px] px-[16px] py-[8px] rounded-[8px]  bg-[#DEDEDE] w-[60px] h-[32px] 
                 }`}
-                // onClick={() => setData({ ...data, summery: text })}
-                // style={{ opacity: text === data?.summery ? 0.5 : 1 }}
+              // onClick={() => setData({ ...data, summery: text })}
+              // style={{ opacity: text === data?.summery ? 0.5 : 1 }}
               >
                 Save
               </button>
--- pages/coverLetter/index.jsx
+++ pages/coverLetter/index.jsx
@@ -38,6 +38,7 @@ function Index() {
     specialization: "",
     university: "",
     letterDate: "",
+    passages:""
   };
 
   const [data, setData] = useState(defaultState);
