adc1910a6580f2c1acd569fff5ce641ff3511464
Asmita Tagad <asmitatagad07@gmail.com>
2024-07-09 19:01:06 +0530

    commit

--- components/common/CoverPdfViewer.jsx
+++ components/common/CoverPdfViewer.jsx
@@ -2,41 +2,26 @@ import React, { useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 
 function CoverPdfViewer({ pdfUrl }) {
-
   const [numPages, setNumPages] = useState(null);
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
   }
- 
+
   return (
     <div
       style={{
         width: "600px",
         height: "75vh",
-      
         display: "flex",
         justifyContent: "center",
         alineItems: "center",
-
-     
         overflow: "scroll",
       }}
     >
-      <Document
-      
-        file={pdfUrl}
-        onLoadSuccess={onDocumentLoadSuccess}
-      >
-        {Array.from(
-          new Array(numPages),
-          (el, index) => (
-            <Page
-              key={`page_${index + 1}`}
-              pageNumber={index + 1}
-              width={590}
-            />
-          )
-        )}
+      <Document file={pdfUrl} onLoadSuccess={onDocumentLoadSuccess}>
+        {Array.from(new Array(numPages), (el, index) => (
+          <Page key={`page_${index + 1}`} pageNumber={index + 1} width={590} />
+        ))}
       </Document>
     </div>
   );
