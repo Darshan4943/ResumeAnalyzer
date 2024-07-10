78021f91403f4d4e5a006d8d61d0804367112c39
Pranay <pranaysalunke5@gmail.com>
2024-07-10 18:33:52 +0530

    scroll arrow

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,13 +1,81 @@
-import React, { useEffect, useRef, useState } from "react";
+// import React, { useEffect, useRef, useState } from "react";
+// import { useRouter } from "next/router";
+// import { popupVisible, reCallUserData } from "../../Redux/actions/user";
+// import { useDispatch, useSelector } from "react-redux";
+// import CandidateHome from "../../components/featured/candidate";
+// import Dashboard from "../dashboard";
+// import PlanExpiredModal from "../../components/models/planExpiredModal";
+// import AdminDashboard from "../dashboard/adminDashboard";
+// import MiniLoader from "../../components/common/miniLoader";
+// import ResetPasswordModal from "../../components/models/resetPasswordModal";
+
+// function BeforeLoginHome() {
+//   const [isLogin, setIsLogin] = useState(false);
+//   const dispatch = useDispatch();
+//   const userDataGlobal = useSelector((state) => state.userData);
+
+//   const [visible, setVisible] = useState(false);
+//   const [loading, setLoading] = useState(true);
+//   useEffect(() => {
+//     const token = localStorage.getItem("authToken");
+//     if (token && token != "undefined") {
+//       if (token) {
+//         setIsLogin(true);
+//       } else {
+//         setIsLogin(false);
+//       }
+//     }
+
+//     const timer = setTimeout(() => {
+//       setLoading(false);
+//       if (userDataGlobal?.tempPassword?.length > 0) {
+//         setVisible(true);
+//       }
+//     }, 1000);
+
+//     return () => clearTimeout(timer);
+//   }, []);
+
+//   const router = useRouter();
+//   const clickHandler = () => {
+//     if (isLogin) {
+//       router.push("/home/BuildResume");
+//     } else {
+//       dispatch(popupVisible());
+//     }
+//   };
+//   return loading ? (
+//     <div className="h-[60vh] w-full flex items-center justify-center">
+//       <MiniLoader />
+//     </div>
+//   ) : (
+//     <div className="">
+//       <PlanExpiredModal />
+//       {isLogin ? (
+//         userDataGlobal?.role == "admin" ? (
+//           <AdminDashboard />
+//         ) : (
+//           <Dashboard />
+//         )
+//       ) : (
+//         <CandidateHome />
+//       )}
+//     </div>
+//   );
+// }
+
+// export default BeforeLoginHome;
+
+
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { popupVisible, reCallUserData } from "../../Redux/actions/user";
+import { popupVisible } from "../../Redux/actions/user";
 import { useDispatch, useSelector } from "react-redux";
 import CandidateHome from "../../components/featured/candidate";
 import Dashboard from "../dashboard";
 import PlanExpiredModal from "../../components/models/planExpiredModal";
 import AdminDashboard from "../dashboard/adminDashboard";
 import MiniLoader from "../../components/common/miniLoader";
-import ResetPasswordModal from "../../components/models/resetPasswordModal";
 
 function BeforeLoginHome() {
   const [isLogin, setIsLogin] = useState(false);
@@ -16,14 +84,12 @@ function BeforeLoginHome() {
 
   const [visible, setVisible] = useState(false);
   const [loading, setLoading] = useState(true);
+  const [showScrollImage, setShowScrollImage] = useState(false);
+
   useEffect(() => {
     const token = localStorage.getItem("authToken");
-    if (token && token != "undefined") {
-      if (token) {
-        setIsLogin(true);
-      } else {
-        setIsLogin(false);
-      }
+    if (token && token !== "undefined") {
+      setIsLogin(!!token);
     }
 
     const timer = setTimeout(() => {
@@ -44,6 +110,11 @@ function BeforeLoginHome() {
       dispatch(popupVisible());
     }
   };
+
+  const handleScrollDown = (scrollingDown) => {
+    setShowScrollImage(scrollingDown);
+  };
+
   return loading ? (
     <div className="h-[60vh] w-full flex items-center justify-center">
       <MiniLoader />
@@ -52,16 +123,29 @@ function BeforeLoginHome() {
     <div className="">
       <PlanExpiredModal />
       {isLogin ? (
-        userDataGlobal?.role == "admin" ? (
+        userDataGlobal?.role === "admin" ? (
           <AdminDashboard />
         ) : (
           <Dashboard />
         )
       ) : (
-        <CandidateHome />
+        <>
+          {showScrollImage && (
+            <div className="relative">
+              <img
+                src="/images/ar.png"
+                alt=""
+                className="fixed top-[90vh] right-[2vw] z-[300000] h-[52px] w-[52px] cursor-pointer"
+                onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+              />
+            </div>
+          )}
+          <CandidateHome onScrollToTop={handleScrollDown} />
+        </>
       )}
     </div>
   );
 }
 
 export default BeforeLoginHome;
+
