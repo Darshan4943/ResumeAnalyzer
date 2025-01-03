2eb81664c4f342b7dd5b74c0d31f8fdf82828770
Darshan <darshanshinde4943@gmail.com>
2025-01-03 17:36:55 +0530

    commit

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -7,13 +7,15 @@ import React, { useEffect, useState } from "react";
 import StackedBarChart from "../../components/common/StackedBarChart";
 import { useSelector } from "react-redux";
 import { camelCase } from "../../utils/middleware";
+import { useRouter } from "next/router";
+import SubscriptionPlans from "../../components/featured/home/SubscriptionPlans";
 
 function Dashboard({ toggleContentt }) {
   const { profileData } = useSelector((state) => state.profile.profileData);
   const { userDataGlobal } = useSelector((state) => state.user.userData);
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
-
+  const router = useRouter();
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
@@ -231,8 +233,94 @@ function Dashboard({ toggleContentt }) {
       date: "13 July, 2021",
     },
   ];
+
+  const handleNavigation = (page) => {
+    router.push(page);
+  };
+  const loginListRecruiter = [
+    {
+      name: "Create New Resume",
+      imgSrc: "/images/resumeBuilder/createResume.png",
+    },
+    {
+      name: "Create New Cover Letter",
+      imgSrc: "/images/resumeBuilder/cover.png",
+    },
+    { name: "My Clients", imgSrc: "/images/resumeBuilder/my_clients.png" },
+    // { name: "Transform CV", imgSrc: "/images/resumeBuilder/transform_cv.png" },
+    {
+      name: "Job Description Matching",
+      imgSrc: "/images/resumeBuilder/job_description_matching.png",
+    },
+    {
+      name: "My Collection",
+
+      imgSrc: "/images/resumeBuilder/collection.png",
+    },
+    { name: "Ask Krut", imgSrc: "/images/resumeBuilder/bot1.png", new: "New" },
+    { name: "Post Jobs", imgSrc: "/images/resumeBuilder/job.png", new: "New" },
+    { name: "My Purchases", imgSrc: "/images/resumeBuilder/my_purchases.png" },
+  ];
+
+  const list = () => {
+    if (userDataGlobal.role === "user") {
+      return loginListCandidate;
+    } else if (userDataGlobal.role === "recruiter") {
+      return loginListRecruiter;
+    } else return loginListRecruiter;
+  };
+
+  function handleItemClick(itemName) {
+    switch (itemName) {
+      case "Create New Resume":
+        handleNavigation(
+          userDataGlobal.role === "user"
+            ? "/home/BuildResume"
+            : "/myClients/ClientResume"
+        );
+        break;
+      case "Create New Cover Letter":
+        handleNavigation(userDataGlobal.role === "user" ? "/coverLetter" : `/myClients/ClientResume?cover=true`);
+        break;
+      case "My Clients":
+        handleNavigation("/myClients");
+        break;
+      case "Resume":
+        handleNavigation("/home/MyCollection");
+        break;
+      case "Transform CV":
+        handleNavigation("/transform/TransformJob");
+        break;
+      case "Job Description Matching":
+        handleNavigation("/transform/JobMatching");
+        break;
+      case "My Purchases":
+        handleNavigation("/purchase/MyPurchase");
+        break;
+      case "My Website":
+        handleNavigation("/myWebsite");
+        break;
+      case "Ask Krut":
+        handleNavigation("/chatbot");
+        break;
+      case "My Collection":
+        handleNavigation(userDataGlobal.role === "user" ? "/home/MyCollection" : "/collection");
+        break;
+      case "Skill Assessments & Certification":
+        handleNavigation("/home/SkillAssessment");
+        break;
+      case "Search Jobs":
+      case "Post Jobs":
+        handleNavigation(
+          userDataGlobal.role === "user" ? "/jobs/search" : "/jobs/list"
+        );
+        break;
+      default:
+        break;
+    }
+  }
   return (
-    <div className=" ml:h-[80vh] w-[100%]  overflow-y-auto "
+    <div className=" ml:h-[calc(100vh-100px)] w-[100%]  overflow-y-auto "
       style={{ scrollbarWidth: "none" }}
     >
 
@@ -428,7 +516,7 @@ function Dashboard({ toggleContentt }) {
 
       </div>
       <div className="lg:flex lg:flex-row flex flex-col w-full pt-6 justify-between">
-      <div className=" pt-6 lg:pt-0 lg:w-[31.26%]  w-[100%] flex flex-col gap-4 lg:justify-between items-start  ">
+        <div className=" pt-6 lg:pt-0 lg:w-[31.26%]  w-[100%] flex flex-col gap-4 lg:justify-between items-start  ">
           <div
             className="flex justify-between items-center px-6 py-[32px] lg:py-[50px]   w-full"
             style={{
@@ -720,7 +808,7 @@ function Dashboard({ toggleContentt }) {
             </div>
           </div>
         </div>
-       
+
       </div>
       <div className="pt-6">
         <div
@@ -1017,14 +1105,52 @@ function Dashboard({ toggleContentt }) {
       <TablePagination
         rowsPerPageOptions={[5, 10, 15]}
         component="div"
-        className="h-[64px] rounded-b-[12px] py-[12px] px-[16px]  border-t bg-white w-[100%]"
+        className=" rounded-b-[12px] px-[16px]  border-t bg-white w-[100%]"
         count={applicants.length}
         rowsPerPage={rowsPerPage}
         page={page}
         onPageChange={handleChangePage}
         onRowsPerPageChange={handleChangeRowsPerPage}
       />
+      <div className="py-6 px-1 flex flex-col gap-6 ">
+        <p className="text-[20px] font-semibold text-[#333333]">Services</p>
+        <div
+          // style={{ border: "2px solid red" }}
+          className="flex gap-[16px] scr420:gap-12 flex-wrap justify-start "
+        >
+
+          {list().map((item, index) => (
+            <div
+              key={index}
+              className={
+                "job-card relative scr420:w-[162.67px] w-[132px] scr420:h-[154px] h-[124px] scr420:p-4 p-3 cursor-pointer flex flex-col items-center scr420:gap-3 gap-2 justify-center text-center bg-white"
+              }
+              onClick={() => handleItemClick(item.name)}
+            >
+              <img
+                src={item.imgSrc}
+                alt=""
+                className="scr420:w-[48px] w-[40px]  scr420:h-[48px] h-[40px] "
+              />
+
+              <div className="scr420:text-[14px] text-[12px] font-medium">
+                {item.name}
+              </div>
+              {item.new && (
+                <div className=" absolute scr420:right-4  scr420:top-4 right-2 top-2 flex justify-center items-center px-2  py-2 scr420:h-[19px] h-[15px] bg-[#F72C2C] rounded-[4px] text-[#FFF] scr420:text-[12px] text-[10px] font-medium leading-tight">
+                  {item.new}
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="py-6  flex flex-col gap-6">
+        <p className="text-[20px] font-semibold text-[#333333]">Purchase Plans</p>
+        <SubscriptionPlans />
+      </div>
     </div>
+
   );
 }
 
