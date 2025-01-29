52d6b2e977171a515997e5cbffd4bf90f5277a04
Darshan <darshanshinde4943@gmail.com>
2025-01-29 18:56:25 +0530

    commit

--- components/featured/home/Dreamjob.jsx
+++ components/featured/home/Dreamjob.jsx
@@ -11,7 +11,7 @@ function DreamJob({ isLogin }) {
       <div className="customMargins flex ml:flex-row flex-col ml:gap-[2vw]  gap-6 items-center ">
         <div className="ml:w-[40%] w-full  flex ml:justify-end  justify-center items-center ">
           <img
-            src="/images/withoutLogin/dreamJob.png"
+            src="/images/withoutLogin/JobPostingUi.png"
             alt=""
             className={`ml:w-[40vw] xl:w-[370.24px] ml:h-[45vw] scr460:h-[300px] scr460:w-[290px] h-[240px] w-[230px] xl:h-[385.35px] object-contain`}
           />
@@ -19,18 +19,17 @@ function DreamJob({ isLogin }) {
         <div className=" flex flex-col gap-6 ml:w-[60%] xl:w-[50%] w-full text-[#000000] ml:text-start text-center  ">
           <div className="flex flex-col gap-[24px] ml:items-start items-center">
             <div className="font-semibold scr460:text-[24px] text-[18px] leading-tight ">
-               Find Your <span className="text-[#06A9EF]">Dream Job</span>
+            Reach Top Talent with Skilotech&apos;s <span className="text-[#06A9EF]">Job Posting</span>
            
             {" "}
-              With <span className="text-[#06A9EF]">Skilotech</span>
+              <span className="text-[#06A9EF]">Platform</span>
              
             </div>
             <div className="font-medium scr460:text-[14px] text-[12px] text-[#333333] ">
-            Skilotech is committed to helping you achieve your career goals. Our job section is designed to connect you with employers who are looking for talented individuals like you. Whether you&apos;re a recent graduate, an experienced professional, or looking for a career change, Skilotech has the resources to help you find your dream job.
-
+            Skilotech offers a powerful job posting platform designed to help recruiters find and hire the best talent. Our platform is easy to use and allows you to create and manage job postings, track applications, and communicate with candidates seamlessly. With Skilotech, you can reach a wide audience of qualified professionals and streamline your hiring process.
             </div>
             <button onClick={() =>
-                    router.push("/auth?signin=true")
+                    router.push("/auth/Sign_in?role=recruiter")
                 }
                    className="px-9   py-3 bg-[#06A9EF] text-white  rounded-[30px] text-[14px] font-semibold btn_hover_effect  max-w-[155px] h-[42px] leading-tight"
                 >
