91be3b7c4e9d1b7d3cf5d89e9a232f1034dc5ad5
Darshan <darshanshinde4943@gmail.com>
2025-02-04 19:04:30 +0530

    commit

--- components/featured/employer/HiringProgress.jsx
+++ components/featured/employer/HiringProgress.jsx
@@ -1,8 +1,9 @@
 import { AnimatePresence, motion } from 'framer-motion';
 import React, { useEffect, useRef, useState } from 'react';
 import LevelUpdate from '../../models/levelUpdate';
+import { DummyProfileSvg } from '../../../utils/svg';
 
-function HiringProgress({ hiringData = [] }) {
+function HiringProgress({ hiringData = [],jobDetails }) {
     console.log("Hiring Data:", hiringData);
 
     const [openTaskModel, setOpenTaskModel] = useState(false);
@@ -33,52 +34,110 @@ function HiringProgress({ hiringData = [] }) {
         <div className='flex flex-col gap-6 overflow-y-auto scr1024:px-6 px-2'>
             {hiringData.length > 0 ? (
                 hiringData.map((level, index) => (
-                    <div key={level._id} className='flex gap-4'>
-                        <div className='w-[24px]'></div>
-                        <div className='flex flex-col gap-4 w-full'>
-                          
-                            <div className='flex gap-4 justify-between items-center'>
-                                <p className='min-w-[60px]'>Level {level.level}</p>
-                                <div className='h-[1px] w-[90%] bg-[#D6DDEB]'></div>
-                            </div>
+                    <React.Fragment key={level._id}>
+                        {level.level === 1 ? (
+                            // Design for Level 1
+                            <div className='flex gap-4'>
+                                <div className='w-[24px]'></div>
+                                <div className='flex flex-col gap-4 w-full'>
+                                    <div className='flex gap-4 justify-between items-center'>
+                                        <p className='min-w-[60px] text-[16px] font-medium'>Level {level.level}</p>
+                                        <div className='h-[1px] w-[90%] bg-[#D6DDEB]'></div>
+                                    </div>
+                                    <p className='text-[18px] font-semibold'>{level.title}</p>
+                                    <div className='flex scr1150:flex-row flex-col justify-between gap-4 w-[80%]'>
+                                        <div className='flex flex-col gap-1'>
+                                            <div className='flex gap-4 text-[16px] font-medium'>
+                                                <p>Profile Score</p>
+                                                <div className='flex gap-1 items-center'>
+                                                    <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <path d="M14.6887 5.52032L10.7215 4.94376L8.9481 1.34844C8.89966 1.25001 8.81998 1.17032 8.72154 1.12188C8.47466 1.00001 8.17466 1.10157 8.05123 1.34844L6.27779 4.94376L2.3106 5.52032C2.20123 5.53594 2.10123 5.58751 2.02466 5.66563C1.9321 5.76077 1.8811 5.88876 1.88286 6.02148C1.88461 6.1542 1.93899 6.28079 2.03404 6.37344L4.90435 9.17188L4.22623 13.1234C4.21032 13.2154 4.2205 13.3099 4.25559 13.3963C4.29068 13.4828 4.34929 13.5576 4.42477 13.6125C4.50025 13.6673 4.58958 13.6999 4.68263 13.7065C4.77568 13.7131 4.86873 13.6936 4.95123 13.65L8.49966 11.7844L12.0481 13.65C12.145 13.7016 12.2575 13.7188 12.3653 13.7C12.6372 13.6531 12.82 13.3953 12.7731 13.1234L12.095 9.17188L14.9653 6.37344C15.0434 6.29688 15.095 6.19688 15.1106 6.08751C15.1528 5.81407 14.9622 5.56094 14.6887 5.52032Z" fill="#FFB836" />
+                                                    </svg>
+                                                    <p className='font-normal'>{level.score}</p>
+                                                </div>
 
-                        
-                            <p className='text-[18px] font-semibold'>{level.title}</p>
-
-                           
-                            <div className='flex scr1150:flex-row flex-col justify-between gap-4 w-[80%]'>
-                                <div className='flex flex-col gap-1'>
-                                    <div className='flex gap-4 text-[16px] font-medium'>
-                                        <p>Profile Score</p>
-                                        <div className='flex gap-2'>
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                                <path d="M21.2831 8.27584L15.3323 7.411L12.6722 2.01803C12.5995 1.87037 12.48 1.75084 12.3323 1.67818C11.962 1.49537 11.512 1.64771 11.3268 2.01803L8.66668 7.411L2.7159 8.27584C2.55184 8.29928 2.40184 8.37662 2.28699 8.49381C2.14815 8.63651 2.07165 8.8285 2.07428 9.02758C2.07692 9.22666 2.15849 9.41655 2.30106 9.55553L6.60653 13.7532L5.58934 19.6805C5.56549 19.8184 5.58074 19.9602 5.63338 20.0899C5.68602 20.2195 5.77394 20.3318 5.88716 20.4141C6.00038 20.4963 6.13437 20.5452 6.27395 20.5551C6.41352 20.5651 6.5531 20.5357 6.67684 20.4704L11.9995 17.6719L17.3222 20.4704C17.4675 20.5477 17.6362 20.5735 17.7979 20.5454C18.2057 20.4751 18.48 20.0883 18.4097 19.6805L17.3925 13.7532L21.6979 9.55553C21.8151 9.44068 21.8925 9.29068 21.9159 9.12662C21.9792 8.71646 21.6932 8.33678 21.2831 8.27584Z" fill="#FFB836" />
-                                            </svg>
-                                            <p className='font-normal'>{level.score}</p>
+                                            </div>
+                                            <p className={`text-[14px] font-medium text-[#0C8A0A]`}>Verified</p>
                                         </div>
                                     </div>
-                                    <p className={`text-[14px] font-medium ${level.status === "Verified" ? "text-[#0C8A0A]" : "text-red"}`}>{level.status}</p>
+                                    <button
+                                        onClick={() => {
+                                            setSelectedLevel(level);
+                                            setOpenTaskModel(true);
+                                        }}
+                                        className='px-9 py-3 bg-[#06A9EF] rounded-[30px] min-w-[209px] w-[210px] text-[14px] font-semibold text-white'
+                                    >
+                                        Move to Next Level
+                                    </button>
                                 </div>
                             </div>
-
-                            
-                            <button
-                                onClick={() => {
-                                    setSelectedLevel(level);
-                                    setOpenTaskModel(true);
-                                }}
-                                className='px-9 py-3 bg-[#06A9EF] rounded-[30px] min-w-[240px] w-[30%] text-[16px] font-semibold text-white'
-                            >
-                                Move to Next Stage
-                            </button>
-                        </div>
-                    </div>
+                        ) : (
+                            // Design for other levels
+                            <div className='flex gap-4'>
+                                <div className='w-[24px]'></div>
+                                <div className='flex flex-col gap-4 w-full'>
+                                    <div className='flex gap-4 justify-between items-center'>
+                                        <p className='min-w-[60px]'>Level {level.level}</p>
+                                        <div className='h-[1px] w-[90%] bg-[#D6DDEB]'></div>
+                                    </div>
+                                    <div className='flex gap-8 w-full'>
+                                        <div className='flex gap-4 flex-col w-[50%]'>
+                                            <p className='text-[18px] font-semibold'>{level.title}</p>
+                                            <div className='flex flex-col gap-2'>
+                                                <p className='text-[#646464] text-[14px] font-normal'>Interview Date</p>
+                                                <div>10 Dec 2023</div>
+                                            </div>
+                                            {!level.isOnline && (
+                                                <div className='flex flex-col gap-2'>
+                                                    <p className='text-[#646464] text-[14px] font-normal'>Interview Location</p>
+                                                    <div>jsdjb jbhahbjds bhjahajjba hbjajh hbahj jjh hjash</div>
+                                                </div>
+                                            )}
+                                            <button
+                                                onClick={() => {
+                                                    setSelectedLevel(level);
+                                                    setOpenTaskModel(true);
+                                                }}
+                                                className='px-9 py-3 bg-[#06A9EF] rounded-[30px] min-w-[209px] w-[210px] text-[14px] font-semibold text-white'
+                                            >
+                                                Move to Next Level
+                                            </button>
+                                        </div>
+                                        <div className='w-[50%] flex flex-col gap-4'>
+                                            <div className='flex flex-col gap-2'>
+                                                <p className='text-[#646464] text-[14px] font-normal'>Interview Status</p>
+                                                <div className={`px-[10px] py-[3px] rounded-[80px] w-fit ${level.status === "Pending" ? "text-[#FFB836] bg-[#EB85331A]" : level.status === "Conducted" ? "text-[#0C8A0A] bg-[#E2FFE1]" : "text-[#FF6550] bg-[#FF65501A]"}`}>
+                                                    {level.status === "Pending" ? "In Progress" : level.status}
+                                                </div>
+                                            </div>
+                                            {level.status !== "Conducted" && (
+                                                <div className='flex flex-col gap-2'>
+                                                    <p className='text-[#646464] text-[16px] font-normal'>Assigned to</p>
+                                                    <div className='flex gap-4 flex-wrap'>
+                                                        {level.assignTo.map((assignee, i) => (
+                                                            <div key={i} className='flex gap-2'>
+                                                                <DummyProfileSvg />
+                                                                <div className='flex flex-col'>
+                                                                    <p className='text-[14px] font-medium'>{assignee.name}</p>
+                                                                    <p className='text-[12px] font-normal text-[#646464]'>{assignee.role}</p>
+                                                                    <p className='text-[12px] font-normal text-[#646464]'>{assignee.email}</p>
+                                                                </div>
+                                                            </div>
+                                                        ))}
+                                                    </div>
+                                                </div>
+                                            )}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
+                    </React.Fragment>
                 ))
             ) : (
                 <p>No hiring data available</p>
             )}
 
-            {/* Task Popup */}
             <AnimatePresence>
                 {openTaskModel && selectedLevel && (
                     <motion.div
@@ -89,12 +148,12 @@ function HiringProgress({ hiringData = [] }) {
                         ref={taskRef}
                         className='absolute z-10 right-[-1.5%] scr1200:max-w-[60%] scr1200:w-[60%] ml:w-[80%] w-[100%]  ml:top-[-2%] ml:bottom-[0%]'
                     >
-                        <LevelUpdate closeTaskPopup={closeTaskPopup} setSuccessfull={setSuccessfull} setTaskSuccessfull={setTaskSuccessfull} />
+                        <LevelUpdate closeTaskPopup={closeTaskPopup} setSuccessfull={setSuccessfull} setTaskSuccessfull={setTaskSuccessfull} selectedLevel={selectedLevel} setSelectedLevel={setSelectedLevel} jobDetails={jobDetails} />
                     </motion.div>
                 )}
             </AnimatePresence>
 
-            {/* Success Popup */}
+
             {(successfull || taskSuccessfull) && (
                 <>
                     <div className="fixed z-[2000] top-0 left-0 right-0 bottom-0 bg-black opacity-60"></div>
