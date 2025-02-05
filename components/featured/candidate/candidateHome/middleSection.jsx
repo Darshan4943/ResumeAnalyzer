a572097c0ad8e4e26bc0164e22829ae814416c43
sahil <sahil21july@gmail.com>
2025-02-05 17:59:28 +0530

    commit

--- components/featured/candidate/candidateHome/middleSection.jsx
+++ components/featured/candidate/candidateHome/middleSection.jsx
@@ -66,11 +66,11 @@ function MiddleSection() {
     },
   ];
   return (
-    <div className="flex flex-col gap-5 max-w-[548px]">
+    <div className="flex flex-col gap-5 w-full ml:max-w-[548px]">
       <div className="flex gap-4">
         <button
           onClick={() => router.push("/jobs/candidate/AppliedJobs")}
-          className="flex gap-2 rounded-[6px] px-4 py-2 border border-[#C7C7C780] bg-[#FFFFFF] text-[14px] font-semibold items-center leading-tight"
+          className="flex gap-1 rounded-[6px] px-2 py-2 border border-[#C7C7C780] bg-[#FFFFFF] text-[12px] font-semibold items-center leading-tight"
         >
           <svg
             width="24"
--- pages/jobs/candidate/SavedJobs.jsx
+++ pages/jobs/candidate/SavedJobs.jsx
@@ -14,9 +14,8 @@ function SavedJobs({ setLimitPopup,
   const { savedJobIds } = useSelector(
     (state) => state.job.jobData
   );
-  console.log(savedJobIds);
   const [loading, setLoading] = useState(true);
-  const { userDataGlobal, profileData,appliedJobData } = useSelector((state) => state.user.userData);
+  const { userDataGlobal, profileData, appliedJobData } = useSelector((state) => state.user.userData);
   const [totalPages, setTotalpages] = useState(0);
   const [limit, setLimit] = useState(10);
   const [totalCount, setTotalCount] = useState(0);
@@ -31,12 +30,12 @@ function SavedJobs({ setLimitPopup,
   const isViewportBelow600 = useMediaQuery("(max-width:600px)");
 
   const posters = [
-    { img: "/images/withoutLogin/trusted1.png"},
-    { img: "/images/withoutLogin/trusted2.png"},
-    { img: "/images/withoutLogin/trusted3.png"},
-    { img: "/images/withoutLogin/trusted4.png"},
-    { img: "/images/withoutLogin/trusted5.png"},
-  
+    { img: "/images/withoutLogin/trusted1.png" },
+    { img: "/images/withoutLogin/trusted2.png" },
+    { img: "/images/withoutLogin/trusted3.png" },
+    { img: "/images/withoutLogin/trusted4.png" },
+    { img: "/images/withoutLogin/trusted5.png" },
+
 
   ];
 
@@ -83,61 +82,57 @@ function SavedJobs({ setLimitPopup,
     }
   }, [savedJobList]);
 
- 
+
   return (
     <>
       {!loading ?
         <>
           {savedJobList?.length > 0 ?
-            <div className="grid grid-cols-12 gap-[24px] customMargins py-6">
-              <p className="col-span-12 text-[#000000] text-[18px] font-semibold">Jobs You&apos;ve Saved <span className="col-span-12 text-[#000000] text-[18px] font-medium">( {savedJobList?.length} Jobs )</span></p>
+            <div className="flex flex-col gap-[24px] customMargins py-6">
+              <p className=" text-[#000000] text-[18px] font-semibold">Jobs You&apos;ve Saved <span className="col-span-12 text-[#000000] text-[18px] font-medium">( {savedJobList?.length} Jobs )</span></p>
+              <div className="flex gap-[24px]">
+                <div
+                  onClick={() => setIsDescription(true)}
+                  className="w-full"
+                >
+                  <SavedJobCard
+                    miniLoading={miniLoading}
+                    selectedJob={selectedJob}
+                    setIsDescription={setIsDescription}
+                    setSelectedJob={setSelectedJob}
+                    savedJobList={savedJobList}
+                    setLimit={setLimit}
+                    limit={limit}
+                    setTotalpages={setTotalpages}
+                    totalPages={totalPages}
+                    page={page}
+                    setPage={setPage}
+                    appliedJobs={appliedJobData}
+                    getData={getData}
+                    setMiniloading={setMiniloading}
 
-              <div
-                onClick={() => setIsDescription(true)}
-                className="col-span-8"
-              >
-                <SavedJobCard
-                  miniLoading={miniLoading}
-                  selectedJob={selectedJob}
-                  setIsDescription={setIsDescription}
-                  setSelectedJob={setSelectedJob}
-                  savedJobList={savedJobList}
-                  setLimit={setLimit}
-                  limit={limit}
-                  setTotalpages={setTotalpages}
-                  totalPages={totalPages}
-                  page={page}
-                  setPage={setPage}
-                  appliedJobs={appliedJobData}
-                  getData={getData}
-                  setMiniloading={setMiniloading}
-
-                />
-              </div>
-
-              <div className="col-span-4 flex flex-col gap-6 rounded-[12px] bg-[#FFFFFF] p-4 h-fit">
-                <p className="text-[16px] font-semibold">Top Company Jobs</p>
-                <div className="flex gap-[10px] flex-wrap">
-                  {posters?.map((item, index) => (
-                    <div key={index} className="rounded-[6px] border border-[#E3E3E3] py-[10px]">
-                      <img
-                        src={item.img}
-                        alt=""
-                        className="h-[36px] w-[142px]  object-contain "
-
-                      />
-                    </div>
-                  ))}
-
+                  />
                 </div>
 
+                <div className=" hidden ml:flex flex-col gap-6   rounded-[12px] bg-[#FFFFFF] p-4 h-fit">
+                  <p className="text-[16px] font-semibold">Top Company Jobs</p>
+                  <div className="flex gap-[10px] flex-wrap">
+                    {posters?.map((item, index) => (
+                      <div key={index} className="rounded-[6px] border border-[#E3E3E3] py-[10px]">
+                        <img
+                          src={item.img}
+                          alt=""
+                          className="h-[36px] w-[142px]  object-contain "
+
+                        />
+                      </div>
+                    ))}
+
+                  </div>
+
+                </div>
               </div>
-
-
-
-
             </div>
-
             :
             <div className=" object-contain justify-center items-center py-12 w-[100%] flex h-full col-span-12">
               <NoJobs name={"Saved"} />
