f50a4ceafa9d914ef2bced6d0adb4445588e2cdd
Adib Kazi <adibkazi1106@gmail.com>
2025-02-14 17:07:57 +0530

    commit

--- components/featured/candidate/jobs/AllJobCard.jsx
+++ components/featured/candidate/jobs/AllJobCard.jsx
@@ -9,11 +9,9 @@ import MiniLoader from "../../../common/mini-loader";
 import { fetchSavedJobIds } from "../../../../Redux/slices/jobSlice";
 import NormalJobCard from "./NormalJobCard";
 import NoJobs from "./noJobs";
-;
-
 function AllJobCard({
   totalCount,
- 
+
   setLimit,
   limit,
 
@@ -21,17 +19,16 @@ function AllJobCard({
   page,
   setPage,
 
-
   jobData,
 
   miniLoading,
-
 }) {
-
   const [selectedJob, setSelectedJob] = useState();
   const [currentPage, setCurrentPage] = useState(1);
   const { userDataGlobal } = useSelector((state) => state.user.userData);
-  const { appliedJobData, savedJobIds } = useSelector((state) => state.job.jobData);
+  const { appliedJobData, savedJobIds } = useSelector(
+    (state) => state.job.jobData
+  );
   const isLogin = useSelector((state) => state.auth.isLogin);
   const dispatch = useDispatch();
   // const [isLogin, setIsLogin] = useState(false);
@@ -48,48 +45,55 @@ function AllJobCard({
   //   }
   // }, []);
 
-
   const posters = [
     {
       title: "Craft a Winning CV in Minutes with AI",
-      description: "Don't let a subpar CV hold you back. Our AI-powered CV builder creates professional, impactful CVs that land you interviews.",
-      path: "/home",
+      description:
+        "Don't let a subpar CV hold you back. Our AI-powered CV builder creates professional, impactful CVs that land you interviews.",
+      path1: "/createResume/BuildResume",
+
+      path2: "/auth/Sign_in?role=user",
       img: "/images/jobs/posters/poster1.png",
       isBgImg: false,
 
-      background: " linear-gradient(178.88deg, #FFFFFF -24.19%, #FFDF9B 131.8%)",
+      background:
+        " linear-gradient(178.88deg, #FFFFFF -24.19%, #FFDF9B 131.8%)",
       backgroundImg: "",
       no: 2,
       page: 1,
-      isCircle: true
+      isCircle: true,
     },
     {
       title: "Skill Assessmentt",
-      description: "Uncover Your Potential with Effortless Skill Assessment. Elevate Your Abilities, Elevate Your Success!",
-      path: "/about",
+      description:
+        "Uncover Your Potential with Effortless Skill Assessment. Elevate Your Abilities, Elevate Your Success!",
+        path1: "/candidate/SkillAssessment",
+        path2: "/auth/Sign_in?role=user",
       img: "/images/jobs/posters/poster2.png",
-      background: " linear-gradient(178.88deg, #FFFFFF -24.19%, #FFDF9B 131.8%)",
+      background:
+        " linear-gradient(178.88deg, #FFFFFF -24.19%, #FFDF9B 131.8%)",
       backgroundImg: "/images/jobs/posters/bg1.png",
       isBgImg: true,
 
       no: 2,
-      page: 2
+      page: 2,
     },
     {
       title: "Contact",
       description: "Get in touch with us",
-      path: "/contact",
-      background: " linear-gradient(178.88deg, #FFFFFF -24.19%, #FFDF9B 131.8%)",
+      path1: "/contact",
+      path2: "/auth/Sign_in?role=user",
+      background:
+        " linear-gradient(178.88deg, #FFFFFF -24.19%, #FFDF9B 131.8%)",
       img: "/images/jobs/posters/poster3.png",
       backgroundImg: "/images/jobs/posters/bg2.png",
       isBgImg: true,
 
       no: 2,
-      page: 3
-    }
+      page: 3,
+    },
   ];
   const pagePoster = posters.find((poster) => poster.page === page);
-
 
   const combinedData = jobData?.reduce((acc, job, index) => {
     acc.push(job);
@@ -100,7 +104,6 @@ function AllJobCard({
 
     return acc;
   }, []);
-
 
   const nextPage = (e) => {
     e.stopPropagation();
@@ -118,72 +121,71 @@ function AllJobCard({
     }
   };
   const handleChange = (e) => {
-
     setLimit(parseInt(e.target.value));
     setPage(1);
     setCurrentPage(1);
   };
 
-
-
   return (
     <div className="flex flex-col gap-4 w-full max-w-[548px] items-center">
-
-      {totalCount > 0 ?
+      {totalCount > 0 ? (
         <>
-
           <div className="flex flex-col gap-4 w-full">
             {combinedData?.map((item, index) => {
               if (item._id) {
-
                 return (
-                  < div key={index}>
+                  <div key={index}>
                     <NormalJobCard item={item} />
-
                   </div>
-                )
-              }
-              else {
+                );
+              } else {
                 return (
-                  <div style={{
-                    background: item.isBgImg
-                      ? `url(${item.backgroundImg}) center/cover no-repeat`
-                      : item.background
-
-                  }}
+                  <div
+                    style={{
+                      background: item.isBgImg
+                        ? `url(${item.backgroundImg}) center/cover no-repeat`
+                        : item.background,
+                    }}
                     key={index}
-                    className='p-[16px] flex   sm:flex-row flex-col-reverse relative scr1100:gap-[80px] gap-8  justify-between items-center  rounded-[12px]  min-h-[169px] overflow-hidden ' >
-                    <div className='flex flex-col gap-2 sm:items-start items-center justify-between h-full'>
-                      <div className='flex flex-col gap-2 sm:items-start items-center '>
-                        <p className=' text-[16px] font-[600] sm:text-start text-center'>
+                    className="p-[16px] flex   sm:flex-row flex-col-reverse relative scr1100:gap-[80px] gap-8  justify-between items-center  rounded-[12px]  min-h-[169px] overflow-hidden "
+                  >
+                    <div className="flex flex-col gap-2 sm:items-start items-center justify-between h-full">
+                      <div className="flex flex-col gap-2 sm:items-start items-center ">
+                        <p className=" text-[16px] font-[600] sm:text-start text-center">
                           {item?.title}
                         </p>
-                        <p className='text-[10px] font-[500] sm:text-start text-center'> {item?.description}</p>
+                        <p className="text-[10px] font-[500] sm:text-start text-center">
+                          {" "}
+                          {item?.description}
+                        </p>
                       </div>
-                      <button className='scr1100:py-3 scr1100:px-9 px-6 py-2 text-[#FFFFFF] bg-blue text-[14px] leading-tight font-[600] rounded-[30px] scr1100:h-[42px] h-[34px] scr1100:w-[155px]'>
+                      <button
+                        onClick={() => router.push(isLogin ?(posters[0].path1): ((posters[0].path2)))}
+                        className="scr1100:py-3 scr1100:px-9 px-6 py-2 text-[#FFFFFF] bg-blue text-[14px] leading-tight font-[600] rounded-[30px] scr1100:h-[42px] h-[34px] scr1100:w-[155px]"
+                      >
                         Know More
                       </button>
                     </div>
-                    {item?.isCircle &&
-                      <div style={{
-                        background: "linear-gradient(249.4deg, #FCE1AA 10.64%, #FFE8B5 98.56%)"
-                      }} className=" h-[206px] w-[206px] rounded-[206px] absolute right-[-50px] top-[-20px]   sm:block hidden">
-
-                      </div>
-                    }
+                    {item?.isCircle && (
+                      <div
+                        style={{
+                          background:
+                            "linear-gradient(249.4deg, #FCE1AA 10.64%, #FFE8B5 98.56%)",
+                        }}
+                        className=" h-[206px] w-[206px] rounded-[206px] absolute right-[-50px] top-[-20px]   sm:block hidden"
+                      ></div>
+                    )}
                     <img
                       src={item?.img}
                       alt=""
                       className="h-[118px] w-[118px]  object-cover z-[10]"
-
                     />
                   </div>
-
-                )
+                );
               }
             })}
           </div>
-          {totalCount > 10 &&
+          {totalCount > 10 && (
             <div className="sm:px-[16px] px-0 w-full justify-between flex ">
               <div className="flex items-center sm:gap-4 gap-2">
                 <p className="text-[14px] text-[#646464] font-600">View</p>
@@ -196,7 +198,6 @@ function AllJobCard({
                     onChange={(e) => handleChange(e)}
                     className="text-[14px] px-[16px] py-[10px] border-[1px] border-[#DEDEDE] bg-[#F9F9F9] rounded-[6px] text-[#333] font-600"
                   >
-
                     <option value="10">10</option>
                     <option value="15">15</option>
                     <option value="20">20</option>
@@ -260,7 +261,9 @@ function AllJobCard({
                     <g clip-path="url(#clip0_2529_10530)">
                       <path
                         d="M9.375 6L15.625 12L9.375 18"
-                        stroke={currentPage !== totalPages ? "#333333" : "#646464"}
+                        stroke={
+                          currentPage !== totalPages ? "#333333" : "#646464"
+                        }
                         stroke-width="2"
                         stroke-linecap="round"
                         stroke-linejoin="round"
@@ -275,11 +278,11 @@ function AllJobCard({
                 </button>
               </div>
             </div>
-          }
+          )}
         </>
-        :
+      ) : (
         <NoJobs />
-      }
+      )}
     </div>
   );
 }
