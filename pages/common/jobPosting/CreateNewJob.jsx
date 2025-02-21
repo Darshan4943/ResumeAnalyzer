ce3677c730b80fc313cc7409a24f58d7cfbd2722
Adib Kazi <adibkazi1106@gmail.com>
2025-02-21 13:13:59 +0530

    commit

--- pages/common/jobPosting/CreateNewJob.jsx
+++ pages/common/jobPosting/CreateNewJob.jsx
@@ -241,11 +241,6 @@ function CreateNewJob() {
     if (reqId) {
       formData.append("reqId", reqId);
     }
-    if (reqId) {
-      formData.append("reqId", reqId);
-    }
-    if (userDataGlobal?.companyId) {
-      formData.append("companyId", userDataGlobal?.companyId);
     if (userDataGlobal?.companyId) {
       formData.append("companyId", userDataGlobal?.companyId);
     }
@@ -288,7 +283,8 @@ function CreateNewJob() {
       );
     }
   };
-    }
+
+
     
   const getData = () => {
     setLoading(true);
