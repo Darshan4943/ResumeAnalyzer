386651712fcaa04ebe4175f5c055684abc1d0ddb
sahil <sahil21july@gmail.com>
2025-02-25 13:38:45 +0530

    commit

--- pages/jobs/candidate/index.jsx
+++ pages/jobs/candidate/index.jsx
@@ -471,12 +471,12 @@ function Index() {
 
             <div className=" flex-col gap-6 rounded-[12px] scr900:flex hidden">
               <img
-                src="/images/jobs/jobPoster1.png"
+                src="/images/home/SearchJobPoster1.png"
                 alt=""
                 className=" scr1100:min-w-[262px] w-[220px] min-w-[200px]  rounded-[12px] scr1100:h-[356px] h-[300px] object-contain "
               />
               <img
-                src="/images/jobs/jobPoster1.png"
+                src="/images/home/SearchJobPoster2.png"
                 alt=""
                 className=" scr1100:min-w-[262px] w-[220px] min-w-[200px] rounded-[12px] scr1100:h-[356px] h-[300px] object-contain "
               />
