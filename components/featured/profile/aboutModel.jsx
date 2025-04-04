b2d5781fee34ccc3b6d70f024a45712c4a4552e7
Adib Kazi <adibkazi1106@gmail.com>
2025-04-04 18:05:31 +0530

    commit

--- components/featured/profile/aboutModel.jsx
+++ components/featured/profile/aboutModel.jsx
@@ -63,6 +63,7 @@ const AboutModal = ({ handleImageClick, userData, setIsComponentOpen }) => {
         .catch((err) => {
           setLoading(false);
           console.log(err);
+          
         });
     } else {
       setError("Minimum 100 characters required");
@@ -147,7 +148,7 @@ const AboutModal = ({ handleImageClick, userData, setIsComponentOpen }) => {
               )}
             </div>
             <p className="text-Text-Secondary text-right font-Montserrat text-[12px] md:text-14 font-normal leading-170]">
-            {Math.max(0, 400 - (text?.length ?? 0))}  characters left
+              {Math.max(0, 400 - (text?.length ?? 0))} characters left
             </p>
           </div>
           <div className="w-full flex items-center justify-end gap-3 xxsm:items-center">
--- pages/common/hiring.jsx
+++ pages/common/hiring.jsx
@@ -103,7 +103,7 @@ function Hiring() {
               ...item,
               options: data.companyNames,
             };
-          }else if (item.heading === "Location") {
+          } else if (item.heading === "Location") {
             return {
               ...item,
               options: data.locations,
@@ -149,8 +149,6 @@ function Hiring() {
   const handleFilterChange = (heading, value) => {
     setFilters((prevFilters) => {
       const updatedFilters = { ...prevFilters };
-
-      
 
       if (value) {
         updatedFilters[heading] = value;
@@ -403,9 +401,10 @@ function Hiring() {
                       }
                       placeholder={
                         filter.heading === "JobTitle"
-                        ? "Job Title"
-                        :  filter.heading === "CompanyName"
-                        ? "Company Name" : filter.heading
+                          ? "Job Title"
+                          : filter.heading === "CompanyName"
+                          ? "Company Name"
+                          : filter.heading
                       }
                       isSearchable={true}
                       noOptionsMessage={() => "No options available"}
