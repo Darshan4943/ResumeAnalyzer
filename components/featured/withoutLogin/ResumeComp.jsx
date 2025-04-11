d27d94af6fba6644d2feab0a86d5143e1342d07f
Darshan <darshanshinde4943@gmail.com>
2025-04-11 17:46:32 +0530

    commit

--- components/featured/withoutLogin/ResumeComp.jsx
+++ components/featured/withoutLogin/ResumeComp.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function ResumeComp() {
   const router = useRouter();
   return (
-    <div className="customMargins flex justify-center h-[430px] ms:h-auto w-full ">
+    <div className="customMargins flex justify-center h-fit w-full ">
       <div
         // style={{
         //   background:
@@ -13,13 +13,13 @@ function ResumeComp() {
         className="max-w-[930px] relative overflow-hidden w-full h-full rounded-[12px] bg-homeResume bg-cover bg-no-repeat"
       >
         
-        <div className=" w-full h-full top-0 flex flex-col ms:flex-row py-2 scr930:py-5 px-4 scr930:px-5 items-center justify-between">
-          <div className="w-full ms:w-[66.34%] flex flex-col gap-[12px] scr930:gap-[26px] ">
+        <div className=" w-full h-full top-0 flex flex-col gap-4 ms:flex-row py-4 scr930:py-5 px-4 scr930:px-5 items-center justify-between">
+          <div className="w-full ms:w-[66.34%] ms:items-start  items-center flex flex-col gap-[12px] scr930:gap-[26px] ">
             <div className="flex flex-col gap-2">
-              <p className="text-[16px] scr800:text-[20px] font-[600] text-[#FFFFFF]">
+              <p className="text-[16px] ms:text-start text-center scr800:text-[20px] font-[600] text-[#FFFFFF]">
                 Unlock your career potential with AI-powered skill assessment.
               </p>
-              <p className="text-[12px] scr800:text-[16px] scr900:text-[14px] font-[500] text-[#FFFFFF]">
+              <p className="text-[12px] scr800:text-[16px] scr900:text-[14px] ms:text-start text-center font-[500] text-[#FFFFFF]">
                 ✅ Boost Your Abilities |
                 <br className="block xsm:hidden" />✅ LandYour Dream Job |
                 <br className="block scr570:hidden ms:block scr900:hidden" /> ✅
