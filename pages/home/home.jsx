df77397f8b29bffc999c34242dba508c56c6a946
Asmita Tagad <asmitatagad07@gmail.com>
2025-04-16 10:30:56 +0530

    commit

--- pages/home/home.jsx
+++ pages/home/home.jsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import CandidateHome from "../candidate";
-
 import { useSelector } from "react-redux";
 import Dashboard from "../dashboard";
 import AdminDashboard from "../dashboard/adminDashboard";
+import ResumePage from "./ResumePage";
 
 function Home() {
   const { userDataGlobal } = useSelector((state) => state.user.userData);
   const isLogin = useSelector((state) => state.auth.isLogin);
+
+  const [showResumePage, setShowResumePage] = useState(false);
+
+  const parsedResume = localStorage.getItem("parsedResume");
+
+  const handleCloseResumePage = () => {
+    setShowResumePage(false); 
+  };
+
   return (
     <div>
       {userDataGlobal?.role === "user" ? (
-        <CandidateHome />
+        showResumePage || parsedResume ? (
+          <ResumePage onClose={handleCloseResumePage} />
+        ) : (
+          <CandidateHome />
+        )
       ) : userDataGlobal?.role === "admin" ? (
         <AdminDashboard />
       ) : (
