aedd5c646a6499c2e5958d18784e72dd3f18798d
Darshan <darshanshinde4943@gmail.com>
2025-05-14 10:28:23 +0530

    commit

--- pages/common/hiring/ApplicantProfile.jsx
+++ pages/common/hiring/ApplicantProfile.jsx
@@ -13,14 +13,14 @@ function ApplicantProfile({ jobDetails }) {
               {jobDetails?.details?.personal?.lastName}
             </p>
           </div>
-          {jobDetails?.details?.personal?.currentLocation &&
+          {jobDetails?.details?.personal?.currentLocation && (
             <div>
               <p className=" text-[14px] font-medium">Address</p>
               <p className="text-[12px] font-normal">
                 {jobDetails?.details?.personal?.currentLocation}
               </p>
             </div>
-          }
+          )}
         </div>
         <div className="flex flex-col gap-4 w-[50%] ">
           {jobDetails?.details?.personal?.gender && (
@@ -40,15 +40,17 @@ function ApplicantProfile({ jobDetails }) {
       <div className="h-[1px] w-full bg-[#D6DDEB]"></div>
       <div className="flex flex-col gap-4">
         <p className="font-semibold">Professional Info</p>
-        {jobDetails?.details?.professional?.aboutme &&
+        {jobDetails?.details?.professional?.aboutme && (
           <div className="flex flex-col gap-2 ml:w-[80%] w-[100%]">
             <p className=" font-medium text-[14px] ">About Me</p>
             <div className="flex flex-col gap-4 text-[12px] font-normal">
               <p>{jobDetails?.details?.professional?.aboutme}</p>
             </div>
-          </div>}
+          </div>
+        )}
         <div className="flex ml:flex-row gap-4 flex-col justify-between">
-          {jobDetails?.details?.professional?.currentJob?.length > 0 || jobDetails?.details?.professional?.hightestQul?.length > 0 ? (
+          {jobDetails?.details?.professional?.currentJob?.length > 0 ||
+          jobDetails?.details?.professional?.hightestQul?.length > 0 ? (
             <div className="flex flex-col gap-4 ml:w-[30%] w-[100%]">
               {jobDetails?.details?.professional?.currentJob && (
                 <div>
@@ -60,7 +62,9 @@ function ApplicantProfile({ jobDetails }) {
               )}
               {jobDetails?.details?.professional?.hightestQul && (
                 <div>
-                  <p className="text-[14px] font-medium">Highest Qualification</p>
+                  <p className="text-[14px] font-medium">
+                    Highest Qualification
+                  </p>
                   <p className="text-[12px] font-normal">
                     {jobDetails?.details?.professional?.hightestQul}
                   </p>
@@ -70,31 +74,32 @@ function ApplicantProfile({ jobDetails }) {
           ) : null}
 
           <div className="flex flex-col gap-4 ml:w-[70%] w-[100%]">
-            {jobDetails?.details?.professional?.totalExperience
-              &&
+            {jobDetails?.details?.professional?.totalExperience && (
               <div>
                 <p className="text-[14px]  font-medium">Experience in Years</p>
                 <p className="text-[12px] font-normal">
                   {jobDetails?.details?.professional?.totalExperience}
                 </p>
               </div>
-            }
+            )}
             {jobDetails?.details?.professional?.skills.length > 0 && (
               <div className="flex flex-col gap-2">
                 <p className=" font-medium">Skills</p>
                 <div className="flex flex-wrap gap-2">
-                  {jobDetails?.details?.professional?.skills
-                    ?.slice(0, 5)
-                    ?.map((item, index) => (
+                  {jobDetails?.details?.professional?.skills?.map(
+                    (item, index) => (
                       <div
                         key={index}
                         className="flex gap-2 font-medium flex-wrap"
                       >
                         <p className="border text-[12px] font-medium border-[#06A9EF] rounded-[24px] px-4 py-2">
-                          {item}
+                          {typeof item === "object" && item !== null
+                            ? item.skill
+                            : item}
                         </p>
                       </div>
-                    ))}
+                    )
+                  )}
                 </div>
               </div>
             )}
