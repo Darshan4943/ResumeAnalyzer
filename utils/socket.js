7e2cb4d811aa40f82aaccf021ff4117f533a503f
Darshan <darshanshinde4943@gmail.com>
2025-07-07 13:27:55 +0530

    commit

--- utils/socket.js
+++ utils/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const socket = io(
   typeof window !== "undefined" && window.location.hostname === "localhost"
-    ? "https://api.skilotech.com"
+    ? "http://localhost:2000"
     : "https://api.skilotech.com", // replace with your domain
   {
     autoConnect: false,
