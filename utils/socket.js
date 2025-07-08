eebab2971323aec0b92ed3e89eacf24ca08fcecb
Prathmesh Jadhav <prasadjadhav1014@gmail.com>
2025-07-08 14:09:09 +0530

    commit

--- utils/socket.js
+++ utils/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const socket = io(
   typeof window !== "undefined" && window.location.hostname === "localhost"
-    ? "http://localhost:2000"
+    ? "https://api.skilotech.com"
     : "https://api.skilotech.com", 
   {
     autoConnect: false,
