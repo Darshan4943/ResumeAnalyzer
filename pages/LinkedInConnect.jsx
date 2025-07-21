577074b0863d4d0f68ca3a78c25c1bf141029317
Darshan <darshanshinde4943@gmail.com>
2025-07-21 13:44:03 +0530

    commit

--- pages/LinkedInConnect.jsx
+++ pages/LinkedInConnect.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const LinkedInConnect = () => {
  const handleConnect = () => {
   const clientId = "866gq2fsy7j6ib"; 
-  const redirectUri = encodeURIComponent("http://localhost:3000/linkedin/callback");
+  const redirectUri = encodeURIComponent("https://www.skilotech.com/linkedin/callback");
   const scope = "r_liteprofile r_emailaddress w_member_social";
 
   const authUrl = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}`;
